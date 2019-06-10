eee053766356722636b830996f063d56fa184b87
samyuu <samyuu@users.noreply.github.com>
2019-06-10 11:02:48 +0200

    Combine Renderbuffer, Framebuffer and RenderTarget

--- Sandbox/src/Graphics/RenderTarget.cpp
+++ Sandbox/src/Graphics/RenderTarget.cpp
@@ -1,4 +1,98 @@
+#include "../pch.h"
 #include "RenderTarget.h"
+
+// ------------------------------------------------------------------------------------------------
+// --- Renderbuffer:
+// ------------------------------------------------------------------------------------------------
+
+Renderbuffer::Renderbuffer()
+{
+}
+
+Renderbuffer::~Renderbuffer()
+{
+	Dispose();
+}
+
+void Renderbuffer::Initialize()
+{
+	glGenRenderbuffers(1, &renderbufferID);
+}
+
+void Renderbuffer::Bind()
+{
+	glBindRenderbuffer(GetRenderTarget(), renderbufferID);
+}
+
+void Renderbuffer::UnBind()
+{
+	glBindRenderbuffer(GetRenderTarget(), NULL);
+}
+
+void Renderbuffer::RenderbufferStorage(int width, int height, InternalFormat_t internalFormat)
+{
+	glRenderbufferStorage(GetRenderTarget(), internalFormat, width, height);
+}
+
+void Renderbuffer::Dispose()
+{
+	if (renderbufferID != NULL)
+		glDeleteRenderbuffers(1, &renderbufferID);
+}
+
+// ------------------------------------------------------------------------------------------------
+// --- Framebuffer:
+// ------------------------------------------------------------------------------------------------
+
+Framebuffer::Framebuffer()
+{
+}
+
+Framebuffer::~Framebuffer()
+{
+	Dispose();
+}
+
+void Framebuffer::Initialize()
+{
+	glGenFramebuffers(1, &framebufferID);
+
+}
+
+void Framebuffer::Bind()
+{
+	glBindFramebuffer(GetBufferTarget(), framebufferID);
+}
+
+void Framebuffer::UnBind()
+{
+	glBindFramebuffer(GetBufferTarget(), NULL);
+}
+
+FramebufferStatus_t Framebuffer::CheckStatus()
+{
+	return glCheckFramebufferStatus(GetBufferTarget());
+}
+
+void Framebuffer::AttachTexture(Texture& texture, Attachment_t attachment)
+{
+	glFramebufferTexture2D(GetBufferTarget(), attachment, texture.GetTextureTarget(), texture.GetTextureID(), 0);
+}
+
+void Framebuffer::AttachRenderbuffer(Renderbuffer& renderbuffer, Attachment_t attachment)
+{
+	glFramebufferRenderbuffer(GetBufferTarget(), attachment, renderbuffer.GetRenderTarget(), renderbuffer.GetRenderbufferID());
+}
+
+void Framebuffer::Dispose()
+{
+	if (framebufferID != NULL)
+		glDeleteFramebuffers(1, &framebufferID);
+}
+
+// ------------------------------------------------------------------------------------------------
+// --- RenderTarget:
+// ------------------------------------------------------------------------------------------------
 
 RenderTarget::RenderTarget()
 {
@@ -52,3 +146,5 @@ void RenderTarget::Dispose()
 {
 
 }
+
+// ------------------------------------------------------------------------------------------------
--- Sandbox/src/Graphics/RenderTarget.h
+++ Sandbox/src/Graphics/RenderTarget.h
@@ -1,7 +1,71 @@
 #pragma once
 #include "../pch.h"
-#include "Renderbuffer.h"
-#include "Framebuffer.h"
+#include "Texture.h"
+
+// ------------------------------------------------------------------------------------------------
+// --- Renderbuffer:
+// ------------------------------------------------------------------------------------------------
+
+typedef GLuint RenderbufferID_t;
+typedef GLenum RenderTarget_t;
+typedef GLenum InternalFormat_t;
+
+class Renderbuffer
+{
+public:
+	Renderbuffer();
+	~Renderbuffer();
+	Renderbuffer(const Renderbuffer&) = delete;
+
+	void Initialize();
+	void Bind();
+	void UnBind();
+	void RenderbufferStorage(int width, int height, InternalFormat_t internalFormat);
+
+	inline RenderbufferID_t GetRenderbufferID() { return renderbufferID; };
+	inline RenderTarget_t GetRenderTarget() { return GL_RENDERBUFFER; };
+
+protected:
+	RenderbufferID_t renderbufferID = NULL;
+
+	void Dispose();
+};
+
+// ------------------------------------------------------------------------------------------------
+// --- Framebuffer:
+// ------------------------------------------------------------------------------------------------
+
+typedef GLuint FramebufferID_t;
+typedef GLenum BufferTarget_t;
+typedef GLenum FramebufferStatus_t;
+typedef GLenum Attachment_t;
+
+class Framebuffer
+{
+public:
+	Framebuffer();
+	~Framebuffer();
+	Framebuffer(const Framebuffer&) = delete;
+
+	void Initialize();
+
+	void Bind();
+	void UnBind();
+
+	FramebufferStatus_t CheckStatus();
+	void AttachTexture(Texture& texture, Attachment_t attachment);
+	void AttachRenderbuffer(Renderbuffer& renderbuffer, Attachment_t attachment);
+
+protected:
+	FramebufferID_t framebufferID = NULL;
+
+	inline BufferTarget_t GetBufferTarget() { return GL_FRAMEBUFFER; };
+	void Dispose();
+};
+
+// ------------------------------------------------------------------------------------------------
+// --- RenderTarget:
+// ------------------------------------------------------------------------------------------------
 
 class RenderTarget
 {
@@ -31,3 +95,4 @@ protected:
 	void Dispose();
 };
 
+// ------------------------------------------------------------------------------------------------
