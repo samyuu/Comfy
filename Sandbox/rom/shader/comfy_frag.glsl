c635bdbab3d639420bca4632b910f651d732ea3a
samyuu <samyuu@users.noreply.github.com>
2019-06-26 06:01:43 +0200

    Implement RGTC2 to RGBA decompression inside the SpriteShader

--- Sandbox/rom/shader/comfy_frag.glsl
+++ Sandbox/rom/shader/comfy_frag.glsl
@@ -7,7 +7,14 @@ in vec2 vertexTexCoord;
 uniform sampler2D textureSampler0;
 uniform sampler2D textureSampler1;
 
+const vec4 ambientColor = vec4(0.90, 0.8, 0.60, 1.0);
+
 void main()
 {
-	fragColor = vertexColor * mix(texture(textureSampler0, vertexTexCoord), texture(textureSampler1, vertexTexCoord), 0.5);
+	// fragColor = vertexColor * texture(textureSampler0, vertexTexCoord);
+	// fragColor = vertexColor * textureLod(textureSampler0, vertexTexCoord, 6);
+	// fragColor = texture(textureSampler0, vertexTexCoord);
+	// fragColor = vertexColor * mix(texture(textureSampler0, vertexTexCoord), texture(textureSampler1, vertexTexCoord), 0.5);
+
+	fragColor = vertexColor * mix(texture(textureSampler0, vertexTexCoord), texture(textureSampler1, vertexTexCoord), 0.5) * ambientColor;
 }
--- Sandbox/rom/shader/sprite_frag.glsl
+++ Sandbox/rom/shader/sprite_frag.glsl
@@ -4,7 +4,7 @@ out vec4 fragColor;
 in vec4 vertexColor;
 in vec2 vertexTexCoord;
 
-uniform float u_textureFormat;
+uniform int u_textureFormat;
 uniform sampler2D textureSampler;
 uniform sampler2D textureMaskSampler;
 
@@ -12,20 +12,33 @@ const vec3 RED_COEF = { +1.5748, 1.0, +0.0000 };
 const vec3 GRN_COEF = { -0.4681, 1.0, -0.1873 };
 const vec3 BLU_COEF = { +0.0000, 1.0, +1.8556 };
 
+const int TextureFormat_RGB = 1;
+const int TextureFormat_RGBA = 2;
+const int TextureFormat_RGBA4 = 5;
+const int TextureFormat_DXT1 = 6;
+const int TextureFormat_DXT3 = 7;
+const int TextureFormat_DXT4 = 8;
+const int TextureFormat_DXT5 = 9;
+const int TextureFormat_ATI1 = 10;
+const int TextureFormat_ATI2 = 11;
+
 void main()
 {
 	vec4 textureColor;
 
-	if (u_textureFormat == 0)
+	if (u_textureFormat == TextureFormat_ATI2)
 	{
-		textureColor = texture(textureSampler, vertexTexCoord);
+		vec4 grayscale = textureLod(textureSampler, vertexTexCoord, 0.0).rrrg;
+		vec4 luminance = textureLod(textureSampler, vertexTexCoord, 1.0) * 1.003922 -0.503929;
+		grayscale.rb = luminance.gr;
+		textureColor.r = dot(grayscale.rgb, RED_COEF);
+		textureColor.g = dot(grayscale.rgb, GRN_COEF);
+		textureColor.b = dot(grayscale.rgb, BLU_COEF);
+		textureColor.a = grayscale.a;
 	}
 	else
 	{
-		vec4 grayscale = textureLod(textureSampler, vertexTexCoord, 0.0).rrrg;
-		vec4 luminance = textureLod(textureSampler, vertexTexCoord, 1.0);
-
-		textureColor = grayscale;
+		textureColor = texture(textureSampler, vertexTexCoord);
 	}
 
 	fragColor = textureColor * vertexColor;
