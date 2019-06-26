c2146a5f6b02fadb92ab90ea74beedfc889d866b
samyuu <samyuu@users.noreply.github.com>
2019-06-26 14:21:43 +0200

    Update SpriteShader TextureFormats

--- Sandbox/rom/shader/sprite_frag.glsl
+++ Sandbox/rom/shader/sprite_frag.glsl
@@ -12,21 +12,40 @@ const vec3 RED_COEF = { +1.5748, 1.0, +0.0000 };
 const vec3 GRN_COEF = { -0.4681, 1.0, -0.1873 };
 const vec3 BLU_COEF = { +0.0000, 1.0, +1.8556 };
 
-const int TextureFormat_RGB = 1;
-const int TextureFormat_RGBA = 2;
+// GL_ALPHA8
+const int TextureFormat_A8 = 0;
+// GL_RGB8
+const int TextureFormat_RGB8 = 1;
+// GL_RGBA8
+const int TextureFormat_RGBA8 = 2;
+// GL_RGB5
+const int TextureFormat_RGB5 = 3;
+// GL_RGB5_A1
+const int TextureFormat_RGB5_A1 = 4;
+// GL_RGBA4
 const int TextureFormat_RGBA4 = 5;
+// GL_COMPRESSED_RGB_S3TC_DXT1_EXT
 const int TextureFormat_DXT1 = 6;
-const int TextureFormat_DXT3 = 7;
-const int TextureFormat_DXT4 = 8;
+// GL_COMPRESSED_RGBA_S3TC_DXT1_EXT
+const int TextureFormat_DXT1a = 7;
+// GL_COMPRESSED_RGBA_S3TC_DXT3_EXT
+const int TextureFormat_DXT3 = 8;
+// GL_COMPRESSED_RGBA_S3TC_DXT5_EXT
 const int TextureFormat_DXT5 = 9;
-const int TextureFormat_ATI1 = 10;
-const int TextureFormat_ATI2 = 11;
-
+// GL_COMPRESSED_RED_RGTC1
+const int TextureFormat_RGTC1 = 10;
+// GL_COMPRESSED_RG_RGTC2
+const int TextureFormat_RGTC2 = 11;
+// GL_LUMINANCE8
+const int TextureFormat_L8 = 12;
+// GL_LUMINANCE8_ALPHA8
+const int TextureFormat_L8A8 = 13;
+	
 void main()
 {
 	vec4 textureColor;
 
-	if (u_textureFormat == TextureFormat_ATI2)
+	if (u_textureFormat == TextureFormat_RGTC2)
 	{
 		vec4 grayscale = textureLod(textureSampler, vertexTexCoord, 0.0).rrrg;
 		vec4 luminance = textureLod(textureSampler, vertexTexCoord, 1.0) * 1.003922 -0.503929;
