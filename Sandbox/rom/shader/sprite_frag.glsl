0126d1776105a6a9c229f40c9fc340444c9e21c3
samyuu <samyuu@users.noreply.github.com>
2019-06-29 09:31:39 +0200

    Clean up SpriteShader text shadow

--- Sandbox/rom/shader/sprite_frag.glsl
+++ Sandbox/rom/shader/sprite_frag.glsl
@@ -61,95 +61,41 @@ vec4 GetTextureColor()
 	return texture(textureSampler, vertexTexCoord);
 }
 
+float GetTextureAlpha(vec2 offset)
+{
+	return (u_textureFormat == TextureFormat_RGTC2) ? 
+		textureLod(textureSampler, vertexTexCoord + offset, 0).g :
+		texture(textureSampler, vertexTexCoord + offset).a;
+}
+
 vec4 GetFontTextureColor()
 {
-	vec2 pixelSize = 1.0 / textureSize(textureSampler, 0);
-	float x = pixelSize.x; float y = pixelSize.y;
+	vec2 size = 1.0 / textureSize(textureSampler, 0);
 
-	// TEMP col, tmp; // TEMP alpha; // MOV alpha, 1.0e-9;
-	vec4 col, alpha;
+	float textureAlpha = GetTextureAlpha(vec2(0.0, 0.0));
+	vec4 shadow = vec4(0.0, 0.0, 0.0, textureAlpha);
 	
-	// TEX tmp, a_tex0, texture[0], 2D;
-	vec4 tmp = texture(textureSampler, vertexTexCoord);
-	
-	// MAX alpha.w, alpha.w, tmp.w;
-	alpha.a = max(alpha.a, tmp.a);
-	
-	// MUL col.xyz, a_color0, tmp.w;
-	col.xyz = vertexColor.rgb * tmp.aaa;
-	
-	// MUL col.xyz, col, a_color0.w; 
-	col.xyz *= vertexColor.w;
-	// TEX tmp, a_tex0, texture[0], 2D, (-1, 0);
-	tmp = texture(textureSampler, vertexTexCoord + vec2(-x, 0));
-	// MAX alpha.x, alpha.x, tmp.w;
-	alpha.x = max(alpha.x, tmp.w);
-	// TEX tmp, a_tex0, texture[0], 2D, (+1, 0);
-	tmp = texture(textureSampler, vertexTexCoord + vec2(+x, 0));
-	//MAX alpha.x, alpha.x, tmp.w;
-	alpha.x = max(alpha.x, tmp.w);
-	// TEX tmp, a_tex0, texture[0], 2D, ( 0,-1);
-	tmp = texture(textureSampler, vertexTexCoord + vec2(0, -y));
-	// MAX alpha.x, alpha.x, tmp.w;
-	alpha.x = max(alpha.x, tmp.w);
-	// TEX tmp, a_tex0, texture[0], 2D, ( 0,+1);
-	tmp = texture(textureSampler, vertexTexCoord + vec2(0, +y));
-	// MAX alpha.x, alpha.x, tmp.w;
-	alpha.x = max(alpha.x, tmp.w);
+	shadow.x = max(shadow.x, GetTextureAlpha(vec2(-size.x, 0.0)));
+	shadow.x = max(shadow.x, GetTextureAlpha(vec2(+size.x, 0.0)));
+	shadow.x = max(shadow.x, GetTextureAlpha(vec2(0.0, -size.y)));
+	shadow.x = max(shadow.x, GetTextureAlpha(vec2(0.0, +size.y)));
 
-	// TEX tmp, a_tex0, texture[0], 2D, (-2, 0);
-	tmp = texture(textureSampler, vertexTexCoord + vec2(-x*2, 0));
-	// MAX alpha.y, alpha.y, tmp.w;
-	alpha.y = max(alpha.y, tmp.w);
-	// TEX tmp, a_tex0, texture[0], 2D, (+2, 0);
-	tmp = texture(textureSampler, vertexTexCoord + vec2(+x*2, 0));
-	// MAX alpha.y, alpha.y, tmp.w;
-	alpha.y = max(alpha.y, tmp.w);
-	// TEX tmp, a_tex0, texture[0], 2D, ( 0,-2);
-	tmp = texture(textureSampler, vertexTexCoord + vec2(0, -y*2));
-	// MAX alpha.y, alpha.y, tmp.w;
-	alpha.y = max(alpha.y, tmp.w);
-	// TEX tmp, a_tex0, texture[0], 2D, ( 0,+2);
-	tmp = texture(textureSampler, vertexTexCoord + vec2(0, +y*2));
-	// MAX alpha.y, alpha.y, tmp.w;
-	alpha.y = max(alpha.y, tmp.w);
+	shadow.y = max(shadow.y, GetTextureAlpha(vec2(-size.x * 2, 0.0)));
+	shadow.y = max(shadow.y, GetTextureAlpha(vec2(+size.x * 2, 0.0)));
+	shadow.y = max(shadow.y, GetTextureAlpha(vec2(0.0, -size.y * 2)));
+	shadow.y = max(shadow.y, GetTextureAlpha(vec2(0.0, +size.y * 2)));
 	
-	// TEX tmp, a_tex0, texture[0], 2D, (-1,-1);
-	tmp = texture(textureSampler, vertexTexCoord + vec2(-x, -y));
-	// MAX alpha.z, alpha.z, tmp.w;
-	alpha.z = max(alpha.z, tmp.w);
-	// TEX tmp, a_tex0, texture[0], 2D, (+1,-1);
-	tmp = texture(textureSampler, vertexTexCoord + vec2(+x, -y));
-	// MAX alpha.z, alpha.z, tmp.w;
-	alpha.z = max(alpha.z, tmp.w);
-	// TEX tmp, a_tex0, texture[0], 2D, (-1,+1);
-	tmp = texture(textureSampler, vertexTexCoord + vec2(-x, +y));
-	// MAX alpha.z, alpha.z, tmp.w;
-	alpha.z = max(alpha.z, tmp.w);
-	// TEX tmp, a_tex0, texture[0], 2D, (+1,+1);
-	tmp = texture(textureSampler, vertexTexCoord + vec2(+x, +y));
-	// MAX alpha.z, alpha.z, tmp.w;
-	alpha.z = max(alpha.z, tmp.w);
+	shadow.z = max(shadow.z, GetTextureAlpha(vec2(-size.x, -size.y)));
+	shadow.z = max(shadow.z, GetTextureAlpha(vec2(+size.x, -size.y)));
+	shadow.z = max(shadow.z, GetTextureAlpha(vec2(-size.x, +size.y)));
+	shadow.z = max(shadow.z, GetTextureAlpha(vec2(+size.x, +size.y)));
 	
-	// MUL alpha, alpha, { 0.9, 0.6, 0.8, 1.0 };
-	alpha *= vec4(0.9, 0.6, 0.8, 1.0);
+	shadow.a = max(shadow.a, shadow.r * 0.9);
+	shadow.a = max(shadow.a, shadow.g * 0.6);
+	shadow.a = max(shadow.a, shadow.b * 0.8);
 
-	// MAX alpha.w, alpha.w, alpha.x;
-	alpha.a = max(alpha.a, alpha.r);
-	// MAX alpha.w, alpha.w, alpha.y;
-	alpha.a = max(alpha.a, alpha.g);
-	// MAX alpha.w, alpha.w, alpha.z;
-	alpha.a = max(alpha.a, alpha.b);
-
-	// RCP tmp.w, alpha.w; ??
-	tmp.w = inversesqrt(alpha.w);
-
-	// MUL o_color.xyz, col, tmp.w;
-	vec4 output = col.xyzw * tmp.wwww;
-	// MUL o_color.w, alpha.w, a_color0.w;
-	output.w = alpha.w * vertexColor.w;
-	
-	return output;
+	vec3 color = vec3((vertexColor.rgb * textureAlpha) * vertexColor.a);
+	return vec4(color * inversesqrt(shadow.a), shadow.a * vertexColor.a);
 }
 
 void main()
