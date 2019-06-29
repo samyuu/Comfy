05b09f735b267a343411a804d99874b737b465de
samyuu <samyuu@users.noreply.github.com>
2019-06-29 07:58:19 +0200

    SpriteVertex implement text shadow rendering

--- Sandbox/rom/shader/sprite_frag.glsl
+++ Sandbox/rom/shader/sprite_frag.glsl
@@ -60,12 +60,110 @@ vec4 GetTextureColor()
 	return texture(textureSampler, vertexTexCoord);
 }
 
+vec4 GetFontTextureColor()
+{
+	vec2 pixelSize = 1.0 / textureSize(textureSampler, 0);
+	float x = pixelSize.x; float y = pixelSize.y;
+
+	// TEMP col, tmp; // TEMP alpha; // MOV alpha, 1.0e-9;
+	vec4 col, alpha;
+	
+	// TEX tmp, a_tex0, texture[0], 2D;
+	vec4 tmp = texture(textureSampler, vertexTexCoord);
+	
+	// MAX alpha.w, alpha.w, tmp.w;
+	alpha.a = max(alpha.a, tmp.a);
+	
+	// MUL col.xyz, a_color0, tmp.w;
+	col.xyz = vertexColor.rgb * tmp.aaa;
+	
+	// MUL col.xyz, col, a_color0.w; 
+	col.xyz *= vertexColor.w;
+	// TEX tmp, a_tex0, texture[0], 2D, (-1, 0);
+	tmp = texture(textureSampler, vertexTexCoord + vec2(-x, 0));
+	// MAX alpha.x, alpha.x, tmp.w;
+	alpha.x = max(alpha.x, tmp.w);
+	// TEX tmp, a_tex0, texture[0], 2D, (+1, 0);
+	tmp = texture(textureSampler, vertexTexCoord + vec2(+x, 0));
+	//MAX alpha.x, alpha.x, tmp.w;
+	alpha.x = max(alpha.x, tmp.w);
+	// TEX tmp, a_tex0, texture[0], 2D, ( 0,-1);
+	tmp = texture(textureSampler, vertexTexCoord + vec2(0, -y));
+	// MAX alpha.x, alpha.x, tmp.w;
+	alpha.x = max(alpha.x, tmp.w);
+	// TEX tmp, a_tex0, texture[0], 2D, ( 0,+1);
+	tmp = texture(textureSampler, vertexTexCoord + vec2(0, +y));
+	// MAX alpha.x, alpha.x, tmp.w;
+	alpha.x = max(alpha.x, tmp.w);
+
+	// TEX tmp, a_tex0, texture[0], 2D, (-2, 0);
+	tmp = texture(textureSampler, vertexTexCoord + vec2(-x*2, 0));
+	// MAX alpha.y, alpha.y, tmp.w;
+	alpha.y = max(alpha.y, tmp.w);
+	// TEX tmp, a_tex0, texture[0], 2D, (+2, 0);
+	tmp = texture(textureSampler, vertexTexCoord + vec2(+x*2, 0));
+	// MAX alpha.y, alpha.y, tmp.w;
+	alpha.y = max(alpha.y, tmp.w);
+	// TEX tmp, a_tex0, texture[0], 2D, ( 0,-2);
+	tmp = texture(textureSampler, vertexTexCoord + vec2(0, -y*2));
+	// MAX alpha.y, alpha.y, tmp.w;
+	alpha.y = max(alpha.y, tmp.w);
+	// TEX tmp, a_tex0, texture[0], 2D, ( 0,+2);
+	tmp = texture(textureSampler, vertexTexCoord + vec2(0, +y*2));
+	// MAX alpha.y, alpha.y, tmp.w;
+	alpha.y = max(alpha.y, tmp.w);
+	
+	// TEX tmp, a_tex0, texture[0], 2D, (-1,-1);
+	tmp = texture(textureSampler, vertexTexCoord + vec2(-x, -y));
+	// MAX alpha.z, alpha.z, tmp.w;
+	alpha.z = max(alpha.z, tmp.w);
+	// TEX tmp, a_tex0, texture[0], 2D, (+1,-1);
+	tmp = texture(textureSampler, vertexTexCoord + vec2(+x, -y));
+	// MAX alpha.z, alpha.z, tmp.w;
+	alpha.z = max(alpha.z, tmp.w);
+	// TEX tmp, a_tex0, texture[0], 2D, (-1,+1);
+	tmp = texture(textureSampler, vertexTexCoord + vec2(-x, +y));
+	// MAX alpha.z, alpha.z, tmp.w;
+	alpha.z = max(alpha.z, tmp.w);
+	// TEX tmp, a_tex0, texture[0], 2D, (+1,+1);
+	tmp = texture(textureSampler, vertexTexCoord + vec2(+x, +y));
+	// MAX alpha.z, alpha.z, tmp.w;
+	alpha.z = max(alpha.z, tmp.w);
+	
+	// MUL alpha, alpha, { 0.9, 0.6, 0.8, 1.0 };
+	alpha *= vec4(0.9, 0.6, 0.8, 1.0);
+
+	// MAX alpha.w, alpha.w, alpha.x;
+	alpha.a = max(alpha.a, alpha.r);
+	// MAX alpha.w, alpha.w, alpha.y;
+	alpha.a = max(alpha.a, alpha.g);
+	// MAX alpha.w, alpha.w, alpha.z;
+	alpha.a = max(alpha.a, alpha.b);
+
+	// RCP tmp.w, alpha.w; ??
+	tmp.w = inversesqrt(alpha.w);
+
+	// MUL o_color.xyz, col, tmp.w;
+	vec4 output = col.xyzw * tmp.wwww;
+	// MUL o_color.w, alpha.w, a_color0.w;
+	output.w = alpha.w * vertexColor.w;
+	
+	return output;
+}
+
 void main()
 {
+	bool u_textShadow = false; // u_textureFormat != TextureFormat_RGTC2;
+
 	if (u_solidColor)
 	{
 		fragColor = vertexColor;
 		return;
+	}
+	else if (u_textShadow)
+	{ 
+		fragColor = GetFontTextureColor(); 
+		return; 
 	}
 
 	fragColor = GetTextureColor() * vertexColor;
