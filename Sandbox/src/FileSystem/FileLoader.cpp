952ebcc90b9c714981b73b2a15ff8ac273b10f2f
samyuu <samyuu@users.noreply.github.com>
2019-07-31 17:42:28 +0200

    Implement basic FileLoader

---
+++ Sandbox/src/FileSystem/FileLoader.cpp
@@ -0,0 +1,134 @@
+#include "FileLoader.h"
+#include "FileHelper.h"
+#include "MemoryStream.h"
+#include "BinaryReader.h"
+#include "Logger.h"
+#include <assert.h>
+
+namespace FileSystem
+{
+	FileLoader::FileLoader()
+	{
+	}
+
+	FileLoader::FileLoader(const std::string& filePath)
+	{
+		SetFilePath(filePath);
+	}
+
+	FileLoader::~FileLoader()
+	{
+		if (threadStarted && loaderThread.joinable())
+			loaderThread.join();
+	}
+
+	void FileLoader::SetFilePath(const std::string& value)
+	{
+		filePath = value;
+	}
+
+	void FileLoader::LoadSync()
+	{
+		assert(!isLoaded);
+		assert(filePath.size() > 0);
+
+		CheckFileLocation();
+		if (!GetFileFound())
+			return;
+
+		if (ReadAllBytes(filePath, &fileContent))
+		{
+			SetIsLoaded(true);
+		}
+	}
+
+	void FileLoader::LoadAsync()
+	{
+		assert(!isLoaded);
+		assert(!threadRunning);
+		assert(filePath.size() > 0);
+
+		CheckFileLocation();
+		if (!GetFileFound())
+			return;
+
+		threadStarted = true;
+		loaderThread = std::thread([this]()
+		{
+			threadRunning = true;
+			if (ReadAllBytes(filePath, &fileContent))
+			{
+				SetIsLoaded(true);
+			}
+			threadRunning = false;
+		});
+	}
+
+	void FileLoader::CheckStartLoadAsync()
+	{
+		if (!isLoaded && !threadStarted && !threadRunning)
+			LoadAsync();
+	}
+
+	bool FileLoader::GetIsLoaded() const
+	{
+		return isLoaded;
+	}
+
+	bool FileLoader::GetFileFound() const
+	{
+		return fileFound;
+	}
+
+	const std::vector<uint8_t>& FileLoader::GetFileContent() const
+	{
+		assert(fileFound && isLoaded);
+		return fileContent;
+	}
+
+	void FileLoader::Read(IBinaryReadable* readable) const
+	{
+		assert(readable != nullptr);
+		assert(fileFound && isLoaded);
+
+		MemoryStream stream(const_cast<std::vector<uint8_t>*>(&fileContent));
+		BinaryReader reader(&stream);
+		
+		readable->Read(reader);
+	}
+
+	void FileLoader::Parse(IBufferParsable* parsable) const
+	{
+		assert(parsable != nullptr);
+		assert(fileFound && isLoaded);
+
+		parsable->Parse(fileContent.data());
+	}
+
+	void FileLoader::FreeData()
+	{
+		assert(fileFound && isLoaded);
+		fileContent.clear();
+		fileContent.shrink_to_fit();
+	}
+
+	void FileLoader::CheckFileLocation()
+	{
+		SetFileFound(FileExists(filePath));
+
+		if (!GetFileFound())
+		{
+			Logger::LogErrorLine(__FUNCTION__ "(): %s not found", filePath.c_str());
+		}
+	}
+
+	void FileLoader::SetIsLoaded(bool value)
+	{
+		isLoaded = true;
+	}
+
+	void FileLoader::SetFileFound(bool value)
+	{
+		fileFound = value;
+	}
+}
---
+++ Sandbox/src/FileSystem/FileLoader.h
@@ -0,0 +1,78 @@
+#pragma once
+#include "Types.h"
+#include "FileInterface.h"
+#include <vector>
+#include <thread>
+
+namespace FileSystem
+{
+	class ISynchronousFileLoader
+	{
+	public:
+		virtual void LoadSync() = 0;
+	};
+
+	class IAsynchronousFileLoader
+	{
+	public:
+		virtual void LoadAsync() = 0;
+		virtual void CheckStartLoadAsync() = 0;
+	};
+
+	class IFileLoader : public ISynchronousFileLoader, public IAsynchronousFileLoader
+	{
+	public:
+		virtual bool GetIsLoaded() const = 0;
+		virtual bool GetFileFound() const = 0;
+
+		virtual const std::vector<uint8_t>& GetFileContent() const = 0;
+		virtual void Read(IBinaryReadable* readable) const = 0;
+		virtual void Parse(IBufferParsable* parsable) const = 0;
+
+		virtual void FreeData() = 0;
+
+	protected:
+		virtual void SetIsLoaded(bool value) = 0;
+		virtual void SetFileFound(bool value) = 0;
+	};
+
+	class FileLoader : public IFileLoader
+	{
+	public:
+		FileLoader();
+		FileLoader(const std::string& filePath);
+		~FileLoader();
+
+		void SetFilePath(const std::string& value);
+
+		virtual void LoadSync() override;
+		virtual void LoadAsync() override;
+		virtual void CheckStartLoadAsync() override;
+
+		virtual bool GetIsLoaded() const override;
+		virtual bool GetFileFound() const override;
+		
+		virtual const std::vector<uint8_t>& GetFileContent() const override;
+		virtual void Read(IBinaryReadable* readable) const override;
+		virtual void Parse(IBufferParsable* parsable) const override;
+
+		virtual void FreeData() override;
+
+	protected:
+		std::string filePath;
+
+		bool isLoaded = false;
+		bool fileFound = false;
+		std::vector<uint8_t> fileContent;
+	
+		bool threadStarted = false;
+		bool threadRunning = false;
+		std::thread loaderThread;
+
+		void CheckFileLocation();
+
+		virtual void SetIsLoaded(bool value) override;
+		virtual void SetFileFound(bool value) override;
+	};
+
+}
