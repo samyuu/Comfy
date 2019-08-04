bda5880b6468fda0d63dbc24fe689cd9c5daa8dd
samyuu <samyuu@users.noreply.github.com>
2019-08-04 04:04:25 +0200

    Add ShaderTestWindow

---
+++ Sandbox/src/DataTest/ShaderTestWindow.cpp
@@ -0,0 +1,126 @@
+#include "ShaderTestWindow.h"
+#include "ImGui/imgui_extensions.h"
+
+namespace DataTest
+{
+	ShaderTestWindow::ShaderTestWindow(Application* parent) : BaseWindow(parent)
+	{
+		CloseWindow();
+	}
+
+	ShaderTestWindow::~ShaderTestWindow()
+	{
+	}
+
+	void ShaderTestWindow::DrawGui()
+	{
+		using namespace ImGui;
+
+		constexpr ImGuiWindowFlags scrollBarWindowFlags = ImGuiWindowFlags_AlwaysVerticalScrollbar | ImGuiWindowFlags_HorizontalScrollbar;
+
+		Text("Shader Test:");
+
+		BeginChild("##ShaderTestWindowListChild", ImVec2(GetWindowWidth() * listWidth, 0), true, scrollBarWindowFlags);
+		{
+			int i = 0;
+			for (auto& shader : ShaderProgram::GetAllShaderPrograms())
+			{
+				PushID(shader);
+				if (ImGui::Selectable(shader->GetShaderName(), i == selectedIndex))
+					selectedIndex = i;
+				i++;
+				PopID();
+			}
+		}
+		EndChild();
+		SameLine();
+		BeginChild("##ShaderTestWindowInfoChild", ImVec2(0, 0), true);
+		{
+			if (selectedIndex >= 0 && selectedIndex < ShaderProgram::GetAllShaderPrograms().size())
+			{
+				ShaderProgram* shader = ShaderProgram::GetAllShaderPrograms()[selectedIndex];
+
+				BulletText("%s	[ OpenGL ID: %d ]", shader->GetShaderName(), shader->GetProgramID());
+				BeginChild("##ShaderTestWindowSourceChild", ImVec2(0, 102), true);
+				{
+					Columns(2);
+
+					Text("Shader Type");
+					NextColumn();
+					Text("Path");
+					NextColumn();
+					Separator();
+
+					PushID(shader->GetVertexShaderPath());
+					Selectable("##ShaderSourceSelectable::ShaderTestWindow", false, ImGuiSelectableFlags_SpanAllColumns);
+					PopID();
+					SameLine();
+
+					Text("Vertex Shader");
+					NextColumn();
+					Text("%s", shader->GetVertexShaderPath());
+					NextColumn();
+
+					PushID(shader->GetVertexShaderPath());
+					Selectable("##ShaderSourceSelectable::ShaderTestWindow", false, ImGuiSelectableFlags_SpanAllColumns);
+					PopID();
+					SameLine();
+
+					Text("Fragment Shader");
+					NextColumn();
+					Text("%s", shader->GetFragmentShaderPath());
+					NextColumn();
+
+					Columns(1);
+
+					if (Button("Reload", ImVec2(GetWindowWidth(), 0.0f)))
+						shader->Recompile();
+				}
+				EndChild();
+
+				BulletText("Uniforms");
+				BeginChild("##ShaderTestWindowUniformChild", ImVec2(0, 0), true);
+				{
+					Columns(3);
+					Text("Data Type");
+					NextColumn();
+					Text("Name");
+					NextColumn();
+					Text("Location");
+					NextColumn();
+					Separator();
+
+					for (Uniform* uniform = shader->GetFirstUniform(); uniform <= shader->GetLastUniform(); uniform++)
+					{
+						PushID(uniform);
+						Selectable("##UniformSelectable::ShaderTestWindow", false, ImGuiSelectableFlags_SpanAllColumns);
+						PopID();
+						SameLine();
+
+						const char* typeName = uniform->GetType() <= UniformType::Count ? typeNames.at(static_cast<int32_t>(uniform->GetType())) : "Unknown";
+						Text("%s", typeName);
+						NextColumn();
+						Text("%s", uniform->GetName());
+						NextColumn();
+						Text("%d", uniform->GetLocation());
+						NextColumn();
+					}
+
+					Columns(1);
+				}
+				EndChild();
+			}
+		}
+		EndChild();
+	}
+
+	const char* ShaderTestWindow::GetGuiName() const
+	{
+		return u8"Shader Test";
+	}
+
+	ImGuiWindowFlags ShaderTestWindow::GetWindowFlags() const
+	{
+		return ImGuiWindowFlags();
+	}
+}
---
+++ Sandbox/src/DataTest/ShaderTestWindow.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "BaseWindow.h"
+#include "Graphics/Shader/ShaderProgram.h"
+#include <array>
+
+namespace DataTest
+{
+	class ShaderTestWindow : public BaseWindow
+	{
+	public:
+		ShaderTestWindow(Application*);
+		~ShaderTestWindow();
+
+		virtual void DrawGui() override;
+		virtual const char* GetGuiName() const override;
+		virtual ImGuiWindowFlags GetWindowFlags() const override;
+
+	private:
+		std::array<const char*, static_cast<size_t>(UniformType::Count)> typeNames =
+		{
+			"Int", 
+			"Float", 
+			"Vec2", 
+			"Vec3", 
+			"Vec4", 
+			"Mat4",
+		};
+
+		const float listWidth = 0.2f;
+
+		int selectedIndex = 0;
+	};
+}
