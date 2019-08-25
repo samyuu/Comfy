ae850b1c725b53e37b29c96eee33632f86c51d74
samyuu <samyuu@users.noreply.github.com>
2019-08-26 01:02:19 +0200

    Update Types.h

--- Sandbox/src/Types.h
+++ Sandbox/src/Types.h
@@ -15,8 +15,13 @@ typedef uint16_t unk16_t;
 typedef uint32_t unk32_t;
 typedef float frame_t;
 
+using ivec2 = glm::ivec2;
+using ivec3 = glm::ivec3;
+using ivec4 = glm::ivec4;
+
 using vec2 = glm::vec2;
 using vec3 = glm::vec3;
 using vec4 = glm::vec4;
+
 using mat3 = glm::mat3;
 using mat4 = glm::mat4;
