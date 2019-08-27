87da7e24000f12b578d88881b2361427f34116f7
samyuu <samyuu@users.noreply.github.com>
2019-08-27 05:03:46 +0200

    Update AetEditor commands

--- Sandbox/src/Editor/Aet/Command/Commands.h
+++ Sandbox/src/Editor/Aet/Command/Commands.h
@@ -2,177 +2,102 @@
 #include "AetCommand.h"
 #include "FileSystem/Format/AetSet.h"
 
+#define DeclarePropertyCommandClass(commandName, commandString, refType, valueType, propertyName)					\
+		class commandName : public AetCommand																		\
+		{																											\
+			RefPtr<refType> ref;																					\
+			valueType newValue, oldValue;																			\
+																													\
+		public:																										\
+			commandName(const RefPtr<refType>& ref, const valueType& value) : ref(ref), newValue(value) {}			\
+																													\
+			void Do()	override			{ oldValue = ref->propertyName; ref->propertyName = newValue; }			\
+			void Undo() override			{ ref->propertyName = oldValue; }										\
+			void Redo() override			{ ref->propertyName = newValue; }										\
+			const char* GetName() override	{ return commandString;			}										\
+		};																											\
+
+#define DeclareAccessorCommandClass(commandName, commandString, refType, valueType, getter, setter)					\
+		class commandName : public AetCommand																		\
+		{																											\
+			RefPtr<refType> ref;																					\
+			valueType newValue, oldValue;																			\
+																													\
+		public:																										\
+			commandName(const RefPtr<refType>& ref, const valueType& value) : ref(ref), newValue(value) {}			\
+																													\
+			void Do()	override			{ oldValue = ref->getter(); ref->setter(newValue); }					\
+			void Undo() override			{ ref->setter(oldValue); }												\
+			void Redo() override			{ ref->setter(newValue); }												\
+			const char* GetName() override	{ return commandString;	 }												\
+		};																											\
+
 namespace Editor::Command
 {
 	using namespace FileSystem;
 
-	namespace AetSet
+	DeclarePropertyCommandClass(AetChangeName, "Change Aet Name", Aet, std::string, Name);
+	DeclarePropertyCommandClass(AetChangeResolution, "Change Resolution", Aet, ivec2, Resolution);
+	DeclarePropertyCommandClass(AetChangeStartFrame, "Change Aet Start Frame", Aet, float, FrameStart);
+	DeclarePropertyCommandClass(AetChangeFrameDuration, "Change Aet Frame Duration", Aet, float, FrameDuration);
+	DeclarePropertyCommandClass(AetChangeFrameRate, "Change Aet Frame Rate", Aet, float, FrameRate);
+	DeclarePropertyCommandClass(AetChangeBackgroundColor, "Change Aet Background Color", Aet, uint32_t, BackgroundColor);
+
+	DeclareAccessorCommandClass(AetObjChangeName, "Change Object Name", AetObj, std::string, GetName, SetName);
+	DeclarePropertyCommandClass(AetObjChangeLoopStart, "Change Object Loop Start", AetObj, float, LoopStart);
+	DeclarePropertyCommandClass(AetObjChangeLoopEnd, "Change Object Loop End", AetObj, float, LoopEnd);
+	DeclarePropertyCommandClass(AetObjChangeStartFrame, "Change Object Start Frame", AetObj, float, StartFrame);
+	DeclarePropertyCommandClass(AetObjChangePlaybackSpeed, "Change Object Playback Speed", AetObj, float, PlaybackSpeed);
+	DeclareAccessorCommandClass(AetObjChangeReferenceRegion, "Change Object Region Reference", AetObj, RefPtr<AetRegion>, GetReferencedRegion, SetReferencedRegion);
+	DeclareAccessorCommandClass(AetObjChangeReferenceLayer, "Change Object Layer Reference", AetObj, RefPtr<AetLayer>, GetReferencedLayer, SetReferencedLayer);
+	DeclareAccessorCommandClass(AetObjChangeObjReferenceParent, "Change Object Reference Parent", AetObj, RefPtr<AetObj>, GetReferencedParentObj, SetReferencedParentObj);
+
+	DeclarePropertyCommandClass(AnimationDataChangeBlendMode, "Change Blend Mode", AnimationData, AetBlendMode, BlendMode);
+	DeclarePropertyCommandClass(AnimationDataChangeUseTextureMask, "Change Use Texture Mask", AnimationData, bool, UseTextureMask);
+
+	DeclarePropertyCommandClass(AetObjChangeMarkerName, "Change Object Marker Name", AetMarker, std::string, Name);
+	DeclarePropertyCommandClass(AetObjChangeMarkerFrame, "Change Object Marker Frame", AetMarker, float, Frame);
+
+	class AetObjAddMarker : public AetCommand
 	{
+		RefPtr<AetObj> ref;
+		RefPtr<AetMarker> newValue;
+	
+	public:
+		AetObjAddMarker(const RefPtr<AetObj>& ref, const RefPtr<AetMarker>& value) : ref(ref), newValue(value) {}
+		void Do()	override { ref->Markers.push_back(newValue); }
+		void Undo() override { ref->Markers.erase(std::find(ref->Markers.begin(), ref->Markers.end(), newValue)); }
+		void Redo() override { Do(); }
+		const char* GetName() override { return "Add Object Marker"; }
+	};
 
-	}
-
-	namespace Aet
+	class AetObjDeleteMarker : public AetCommand
 	{
-		class ChangeName : public AetCommand
-		{
-			RefPtr<FileSystem::Aet> aet;
-			std::string newName, oldName;
+		RefPtr<AetObj> ref;
+		RefPtr<AetMarker> newValue;
+		int index;
 
-		public:
-			ChangeName(const RefPtr<FileSystem::Aet>& aet, const std::string& name) : aet(aet), newName(name) {};
+	public:
+		AetObjDeleteMarker(const RefPtr<AetObj>& ref, int value) : ref(ref), index(value) {}
+		void Do()	override { newValue = ref->Markers.at(index); Redo(); }
+		void Undo() override { ref->Markers.insert(ref->Markers.begin() + index, newValue); }
+		void Redo() override { ref->Markers.erase(ref->Markers.begin() + index); }
+		const char* GetName() override { return "Delete Object Marker"; }
+	};
 
-			void Do() override { oldName = aet->Name; aet->Name = newName; };
-			void Undo() override { aet->Name = oldName; };
-			void Redo() override { aet->Name = newName; };
-			const char* GetName() override { return "Change Aet Name"; };
-		};
-
-		class ChangeStartFrame : public AetCommand
-		{
-			RefPtr<FileSystem::Aet> aet;
-			float newFrame, oldFrame;
-
-		public:
-			ChangeStartFrame(const RefPtr<FileSystem::Aet>& aet, float startFrame) : aet(aet), newFrame(startFrame) {};
-
-			void Do() override { oldFrame = aet->FrameStart; aet->FrameStart = newFrame; };
-			void Undo() override { aet->FrameStart = oldFrame; };
-			void Redo() override { aet->FrameStart = newFrame; };
-			const char* GetName() override { return "Change Aet Start Frame"; };
-		};
-
-		class ChangeFrameDuration : public AetCommand
-		{
-			RefPtr<FileSystem::Aet> aet;
-			float newDuration, oldDuration;
-
-		public:
-			ChangeFrameDuration(const RefPtr<FileSystem::Aet>& aet, float frameDuration) : aet(aet), newDuration(frameDuration) {};
-
-			void Do() override { oldDuration = aet->FrameDuration; aet->FrameDuration = newDuration; };
-			void Undo() override { aet->FrameDuration = oldDuration; };
-			void Redo() override { aet->FrameDuration = newDuration; };
-			const char* GetName() override { return "Change Aet Frame Duration"; };
-		};
-
-		class ChangeFrameRate : public AetCommand
-		{
-			RefPtr<FileSystem::Aet> aet;
-			float newFrameRate, oldFrameRate;
-
-		public:
-			ChangeFrameRate(const RefPtr<FileSystem::Aet>& aet, float frameRate) : aet(aet), newFrameRate(frameRate) {};
-
-			void Do() override { oldFrameRate = aet->FrameRate; aet->FrameRate = newFrameRate; };
-			void Undo() override { aet->FrameRate = oldFrameRate; };
-			void Redo() override { aet->FrameRate = newFrameRate; };
-			const char* GetName() override { return "Change Aet Frame Rate"; };
-		};
-
-		class ChangeResolution : public AetCommand
-		{
-			RefPtr<FileSystem::Aet> aet;
-			int32_t newWidth, newHeight, oldWidth, oldHeight;
-
-		public:
-			ChangeResolution(const RefPtr<FileSystem::Aet>& aet, int width, int height) : aet(aet), newWidth(width), newHeight(height) {};
-
-			void Do() override { oldWidth = aet->Width; oldHeight = aet->Height; aet->Width = newWidth; aet->Height = newHeight; };
-			void Undo() override { aet->Width = oldWidth; aet->Height = oldHeight; };
-			void Redo() override { aet->Width = newWidth; aet->Height = newHeight; };
-			const char* GetName() override { return "Change Resolution"; };
-		};
-
-		class ChangeBackgroundColor : public AetCommand
-		{
-			RefPtr<FileSystem::Aet> aet;
-			uint32_t newColor, oldColor;
-
-		public:
-			ChangeBackgroundColor(const RefPtr<FileSystem::Aet>& aet, uint32_t color) : aet(aet), newColor(color) {};
-
-			void Do() override { oldColor = aet->BackgroundColor; aet->BackgroundColor = newColor; };
-			void Undo() override { aet->BackgroundColor = oldColor; };
-			void Redo() override { aet->BackgroundColor = newColor; };
-			const char* GetName() override { return "Change Aet Background Color"; };
-		};
-	}
-
-	namespace AetLayer
+	class AetObjMoveMarker : public AetCommand
 	{
+		RefPtr<AetObj> ref;
+		std::tuple<int, int> newValue;
 
-	}
-
-	namespace AetObj
-	{
-		class ChangeName : public AetCommand
-		{
-			RefPtr<FileSystem::AetObj> aetObj;
-			std::string newName, oldName;
-
-		public:
-			ChangeName(const RefPtr<FileSystem::AetObj>& aetObj, const std::string& name) : aetObj(aetObj), newName(name) {};
-
-			void Do() override { oldName = aetObj->GetName(); aetObj->SetName(newName); };
-			void Undo() override { aetObj->SetName(oldName); };
-			void Redo() override { aetObj->SetName(newName); };
-			const char* GetName() override { return "Change Object Name"; };
-		};
-
-		class ChangeLoopStart : public AetCommand
-		{
-			RefPtr<FileSystem::AetObj> aetObj;
-			float newStart, oldStart;
-
-		public:
-			ChangeLoopStart(const RefPtr<FileSystem::AetObj>& aetObj, float loopStart) : aetObj(aetObj), newStart(loopStart) {};
-
-			void Do() override { oldStart = aetObj->LoopStart; aetObj->LoopStart = newStart; };
-			void Undo() override { aetObj->LoopStart = oldStart; };
-			void Redo() override { aetObj->LoopStart = newStart; };
-			const char* GetName() override { return "Change Object Loop Start"; };
-		};
-
-		class ChangeLoopEnd : public AetCommand
-		{
-			RefPtr<FileSystem::AetObj> aetObj;
-			float newEnd, oldEnd;
-
-		public:
-			ChangeLoopEnd(const RefPtr<FileSystem::AetObj>& aetObj, float loopEnd) : aetObj(aetObj), newEnd(loopEnd) {};
-
-			void Do() override { oldEnd = aetObj->LoopEnd; aetObj->LoopEnd = newEnd; };
-			void Undo() override { aetObj->LoopEnd = oldEnd; };
-			void Redo() override { aetObj->LoopEnd = newEnd; };
-			const char* GetName() override { return "Change Object Loop End"; };
-		};
-
-		class ChangeStartFrame : public AetCommand
-		{
-			RefPtr<FileSystem::AetObj> aetObj;
-			float newStart, oldStart;
-
-		public:
-			ChangeStartFrame(const RefPtr<FileSystem::AetObj>& aetObj, float startFrame) : aetObj(aetObj), newStart(startFrame) {};
-
-			void Do() override { oldStart = aetObj->StartFrame; aetObj->StartFrame = newStart; };
-			void Undo() override { aetObj->StartFrame = oldStart; };
-			void Redo() override { aetObj->StartFrame = newStart; };
-			const char* GetName() override { return "Change Object Start Frame"; };
-		};
-
-		class ChangePlaybackSpeed : public AetCommand
-		{
-			RefPtr<FileSystem::AetObj> aetObj;
-			float newSpeed, oldSpeed;
-
-		public:
-			ChangePlaybackSpeed(const RefPtr<FileSystem::AetObj>& aetObj, float speed) : aetObj(aetObj), newSpeed(speed) {};
-
-			void Do() override { oldSpeed = aetObj->PlaybackSpeed; aetObj->PlaybackSpeed = newSpeed; };
-			void Undo() override { aetObj->PlaybackSpeed = oldSpeed; };
-			void Redo() override { aetObj->PlaybackSpeed = newSpeed; };
-			const char* GetName() override { return "Change Object Playback Speed"; };
-		};
-	}
+	public:
+		AetObjMoveMarker(const RefPtr<AetObj>& ref, std::tuple<int, int> value) : ref(ref), newValue(value) {}
+		void Do()	override { std::iter_swap(ref->Markers.begin() + std::get<0>(newValue), ref->Markers.begin() + std::get<1>(newValue)); }
+		void Undo() override { std::iter_swap(ref->Markers.begin() + std::get<1>(newValue), ref->Markers.begin() + std::get<0>(newValue)); }
+		void Redo() override { Do(); }
+		const char* GetName() override { return "Move Object Marker"; }
+	};
 }
+
+#undef DeclarePropertyCommandClass
+#undef DeclareAccessorCommandClass
