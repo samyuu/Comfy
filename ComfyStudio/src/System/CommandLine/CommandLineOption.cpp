eeadb0d06e67db48532c9f255b55365eaec83a26
samyuu <samyuu@users.noreply.github.com>
2019-09-04 11:07:19 +0200

    CommandLineOption add farc extractor command

--- ComfyStudio/src/System/CommandLine/CommandLineOption.cpp
+++ ComfyStudio/src/System/CommandLine/CommandLineOption.cpp
@@ -2,9 +2,32 @@
 #include "Core/Logger.h"
 #include "System/Version/BuildVersion.h"
 #include "System/Version/BuildConfiguration.h"
+#include "FileSystem/Archive/Farc.h"
+#include "FileSystem/FileHelper.h"
+#include "Misc/StringHelper.h"
 
 namespace System
 {
+	static void FarcProcessor(int index, const char* arguments[])
+	{
+		using namespace FileSystem;
+
+		const String filePath = arguments[index];
+		RefPtr<Farc> farc = Farc::Open(filePath);
+		if (farc)
+		{
+			const String baseDirectory = GetDirectory(filePath);
+			const String directory = baseDirectory + "\\" + GetFileName(filePath, false);
+			CreateDirectoryFile(Utf8ToUtf16(directory));
+
+			for (const ArchiveEntry& entry : *farc)
+			{
+				Vector<uint8_t> data = entry.ReadVector();
+				WriteAllBytes(directory + String("\\") + entry.Name, data);
+			}
+		}
+	}
+
 	const char* CommandLineOption::GetDescription() const
 	{
 		return (Description != nullptr) ? Description : "No Description";
@@ -31,6 +54,7 @@ namespace System
 			{ "-t",		"--test",			"Print a test message",				true,	0, [](int index, const char* arguments[]) { Logger::LogLine(">> Test Message"); } },
 			{ "-te",	"--test_echo",		"Echo the following argument",		false,	1, [](int index, const char* arguments[]) { Logger::LogLine(">> echo '%s'", arguments[index]); } },
 			{ "-e",		"--exit",			"Exit the application",				true,	0, [](int index, const char* arguments[]) {} },
+			{ "-f",		"--farc",			"Extract Farc",						true,	1, FarcProcessor },
 		};
 	}
 
