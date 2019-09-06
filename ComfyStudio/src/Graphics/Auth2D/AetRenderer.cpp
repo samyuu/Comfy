8e88d96aca5026689cc9b4ef145905f6a0370743
samyuu <samyuu@users.noreply.github.com>
2019-09-06 08:22:58 +0200

    AetRenderer expand interface and add texture mask rendering

--- ComfyStudio/src/Graphics/Auth2D/AetRenderer.cpp
+++ ComfyStudio/src/Graphics/Auth2D/AetRenderer.cpp
@@ -36,8 +36,6 @@ namespace Graphics::Auth2D
 		if (obj.Region == nullptr || !obj.Visible)
 			return;
 
-		// TODO: render texture mask
-
 		const FileSystem::Texture* texture;
 		const FileSystem::Sprite* sprite;
 		bool validSprite = GetSprite(obj.Region->GetSprite(obj.SpriteIndex), &texture, &sprite);
@@ -54,16 +52,91 @@ namespace Graphics::Auth2D
 				vec4(1.0f, 1.0f, 1.0f, obj.Properties.Opacity * opacity),
 				obj.BlendMode);
 		}
+		else
+		{
+			renderer2D->Draw(
+				nullptr,
+				vec4(0, 0, obj.Region->Width, obj.Region->Height),
+				obj.Properties.Position + position,
+				obj.Properties.Origin,
+				obj.Properties.Rotation,
+				obj.Properties.Scale,
+				vec4(DummyColor.r, DummyColor.g, DummyColor.b, DummyColor.a * obj.Properties.Opacity * opacity),
+				obj.BlendMode);
+		}
+	}
+
+	void AetRenderer::RenderObjCacheMask(const AetMgr::ObjCache& maskObj, const AetMgr::ObjCache& obj, const vec2& position, float opacity)
+	{
+		if (maskObj.Region == nullptr || obj.Region == nullptr)
+			return;
+
+		const FileSystem::Texture* maskTexture;
+		const FileSystem::Sprite* maskSprite;
+		bool validMaskSprite = GetSprite(maskObj.Region->GetSprite(maskObj.SpriteIndex), &maskTexture, &maskSprite);
+
+		const FileSystem::Texture* texture;
+		const FileSystem::Sprite* sprite;
+		bool validSprite = GetSprite(obj.Region->GetSprite(obj.SpriteIndex), &texture, &sprite);
+
+		if (validMaskSprite && validSprite)
+		{
+			renderer2D->Draw(
+				maskTexture->GraphicsTexture.get(),
+				maskSprite->PixelRegion,
+				maskObj.Properties.Position,
+				maskObj.Properties.Origin,
+				maskObj.Properties.Rotation,
+				maskObj.Properties.Scale,
+				texture->GraphicsTexture.get(),
+				sprite->PixelRegion,
+				obj.Properties.Position,
+				obj.Properties.Origin,
+				obj.Properties.Rotation,
+				obj.Properties.Scale,
+				vec4(1.0f, 1.0f, 1.0f, obj.Properties.Opacity),
+				obj.BlendMode);
+		}
+		else
+		{
+			renderer2D->Draw(
+				nullptr,
+				vec4(0, 0, obj.Region->Width, obj.Region->Height),
+				obj.Properties.Position + position,
+				obj.Properties.Origin,
+				obj.Properties.Rotation,
+				obj.Properties.Scale,
+				vec4(DummyColor.r, DummyColor.g, DummyColor.b, DummyColor.a * obj.Properties.Opacity * opacity),
+				obj.BlendMode);
+		}
+	}
+
+	void AetRenderer::RenderObjCacheVector(const Vector<AetMgr::ObjCache>& objectCache, const vec2& position, float opacity)
+	{
+		bool singleObject = objectCache.size() == 1;
+
+		for (size_t i = 0; i < objectCache.size(); i++)
+		{
+			const AetMgr::ObjCache& obj = objectCache[i];
+
+			if (obj.UseTextureMask && !singleObject && (i + 1 < objectCache.size()))
+			{
+				RenderObjCacheMask(objectCache[i + 1], obj, position, opacity);
+				i++;
+			}
+			else
+			{
+				RenderObjCache(obj, position, opacity);
+			}
+		}
 	}
 
 	void AetRenderer::RenderAetObj(const AetObj* aetObj, float frame, const vec2& position, float opacity)
 	{
-		objectsCache.clear();
+		objectCache.clear();
 
-		AetMgr::GetAddObjects(objectsCache, aetObj, frame);
-
-		for (auto& objCache : objectsCache)
-			RenderObjCache(objCache, position, opacity);
+		AetMgr::GetAddObjects(objectCache, aetObj, frame);
+		RenderObjCacheVector(objectCache, position, opacity);
 	}
 
 	void AetRenderer::RenderAetObjLooped(const AetObj* aetObj, float frame, const vec2& position, float opacity)
@@ -74,6 +147,21 @@ namespace Graphics::Auth2D
 	void AetRenderer::RenderAetObjClamped(const AetObj* aetObj, float frame, const vec2& position, float opacity)
 	{
 		RenderAetObj(aetObj, (frame >= aetObj->LoopEnd ? aetObj->LoopEnd : frame), position, opacity);
+	}
+
+	void AetRenderer::RenderAetSprite(const AetRegion* aetRegion, const AetSprite* aetSprite, const vec2& position)
+	{
+		const FileSystem::Texture* texture;
+		const FileSystem::Sprite* sprite;
+
+		if (aetRegion->SpriteCount() < 1 || !GetSprite(aetSprite, &texture, &sprite))
+		{
+			renderer2D->Draw(nullptr, vec4(0, 0, aetRegion->Width, aetRegion->Height), vec2(0.0f), vec2(0.0f), 0.0f, vec2(1.0f), AetRenderer::DummyColor);
+		}
+		else
+		{
+			renderer2D->Draw(texture->GraphicsTexture.get(), sprite->PixelRegion, vec2(0.0f), vec2(0.0f), 0.0f, vec2(1.0f), vec4(1.0f));
+		}
 	}
 
 	bool AetRenderer::SpriteNameSprSetSpriteGetter(const SprSet* sprSet, const AetSprite* inSprite, const FileSystem::Texture** outTexture, const Sprite** outSprite)
@@ -93,7 +181,7 @@ namespace Graphics::Auth2D
 		{
 			if (EndsWith(inSprite->Name, sprite.Name))
 			{
-				// temporary
+				// NOTE: Temporary solution, check for IDs in the future
 				inSprite->SpriteCache = &sprite;
 				goto from_sprite_cache;
 			}
--- ComfyStudio/src/Graphics/Auth2D/AetRenderer.h
+++ ComfyStudio/src/Graphics/Auth2D/AetRenderer.h
@@ -25,11 +25,18 @@ namespace Graphics::Auth2D
 		void SetSpriteGetterFunction(SpriteGetterFunction* value);
 		
 	public:
+		static constexpr vec4 DummyColor = vec4(0.79f, 0.90f, 0.57f, 0.50f);
+
 		void RenderObjCache(const AetMgr::ObjCache& obj, const vec2& position, float opacity = 1.0f);
+		void RenderObjCacheMask(const AetMgr::ObjCache& maskObj, const AetMgr::ObjCache& obj, const vec2& position, float opacity = 1.0f);
+		void RenderObjCacheVector(const Vector<AetMgr::ObjCache>& objectCache, const vec2& position = vec2(0.0f, 0.0f), float opacity = 1.0f);
+
 		void RenderAetObj(const AetObj* aetObj, float frame, const vec2& position = vec2(0.0f, 0.0f), float opacity = 1.0f);
 		void RenderAetObjLooped(const AetObj* aetObj, float frame, const vec2& position = vec2(0.0f, 0.0f), float opacity = 1.0f);
 		void RenderAetObjClamped(const AetObj* aetObj, float frame, const vec2& position = vec2(0.0f, 0.0f), float opacity = 1.0f);
 	
+		void RenderAetSprite(const AetRegion* aetRegion, const AetSprite* aetSprite, const vec2& position);
+
 		static bool SpriteNameSprSetSpriteGetter(const SprSet* sprSet, const AetSprite* inSprite, const FileSystem::Texture** outTexture, const Sprite** outSprite);
 
 	protected:
@@ -39,6 +46,6 @@ namespace Graphics::Auth2D
 		Renderer2D* renderer2D = nullptr;
 		SpriteGetterFunction* spriteGetter = nullptr;
 
-		Vector<AetMgr::ObjCache> objectsCache;
+		Vector<AetMgr::ObjCache> objectCache;
 	};
 }
