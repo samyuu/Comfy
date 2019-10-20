cad3959b347149f073aac46c63ba98a363efb01a
samyuu <samyuu@users.noreply.github.com>
2019-10-21 00:07:31 +0200

    TimelineScrollbar fix incorrect grab region behavior

--- ComfyStudio/src/Editor/Timeline/TimelineScrollbar.cpp
+++ ComfyStudio/src/Editor/Timeline/TimelineScrollbar.cpp
@@ -17,31 +17,33 @@ namespace Editor
 		const vec2 scrollbarRegionSize = scrollbarRegion.GetSize();
 		const float scrollbarAxisSize = scrollbarRegionSize[axis];
 
-		const float visibleContentPercentage = (maxScroll <= 0.0f) ? 0.0f : (scrollbarAxisSize / maxScroll);
+		const float visibleContentPercentage = (maxScroll <= 0.0f) ? 0.0f : (scrollbarAxisSize / (maxScroll + scrollbarAxisSize));
 		const float visibleContentSize = glm::clamp(((maxScroll <= 0.0f) ? scrollbarAxisSize : visibleContentPercentage * scrollbarAxisSize), (style.GrabMinSize * 2.0f), scrollbarAxisSize);
+
 		const float avilableSize = scrollbarAxisSize - visibleContentSize;
 		const float grabSizePercentage = (maxScroll <= 0.0f) ? 0.0f : (scroll / maxScroll);
 
-		ImRect scrollbarGrabRegion;
+		const float grabPosition = avilableSize * grabSizePercentage;
 
+		ImRect scrollbarGrabRegion;
 		if (axis == ImGuiAxis_X)
 		{
-			const float grabX = avilableSize * grabSizePercentage;
-
 			constexpr vec2 grabPadding = vec2(2.0f, 3.0f);
 			scrollbarGrabRegion = ImRect(
-				scrollbarRegion.Min + vec2(+grabPadding.x + grabX, grabPadding.y),
-				scrollbarRegion.Min + vec2(-grabPadding.x + grabX + visibleContentSize, timelineScrollbarSize.y - grabPadding.y));
+				scrollbarRegion.Min + vec2(+grabPadding.x + grabPosition, grabPadding.y),
+				scrollbarRegion.Min + vec2(-grabPadding.x + grabPosition + visibleContentSize, timelineScrollbarSize.y - grabPadding.y));
 		}
 		else if (axis == ImGuiAxis_Y)
 		{
-			const float grabY = avilableSize * grabSizePercentage;
-
 			constexpr vec2 grabPadding = vec2(3.0f, 2.0f);
 			scrollbarGrabRegion = ImRect(
-				scrollbarRegion.Min + vec2(+grabPadding.x + 0.0, grabPadding.y + grabY),
-				scrollbarRegion.Min + vec2(-grabPadding.x + timelineScrollbarSize.x, -grabPadding.y + grabY + visibleContentSize));
+				scrollbarRegion.Min + vec2(+grabPadding.x + 0.0, grabPadding.y + grabPosition),
+				scrollbarRegion.Min + vec2(-grabPadding.x + timelineScrollbarSize.x, -grabPadding.y + grabPosition + visibleContentSize));
 		}
+
+		const vec2 scrollbarGrabRegionSize = scrollbarGrabRegion.GetSize();
+		if (scrollbarGrabRegionSize.x < 0.0f || scrollbarGrabRegionSize.y < 0.0f)
+			return;
 
 		drawList->AddRectFilled(scrollbarGrabRegion.Min, scrollbarGrabRegion.Max, GetGrabColor(), style.ScrollbarRounding);
 	}
