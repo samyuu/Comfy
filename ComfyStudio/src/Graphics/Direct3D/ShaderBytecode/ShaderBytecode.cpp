d8e81d071bcfa6c7dcd0f4209d22da0d862cd04e
samyuu <samyuu@users.noreply.github.com>
2019-11-22 04:04:19 +0100

    Move shader bytecode header files into an intermediate directory

--- ComfyStudio/src/Graphics/Direct3D/ShaderBytecode/ShaderBytecode.cpp
+++ ComfyStudio/src/Graphics/Direct3D/ShaderBytecode/ShaderBytecode.cpp
@@ -2,9 +2,9 @@
 
 // NOTE: To automatically resolve the correct include directory based on the build configuration
 #if COMFY_DEBUG
-#define SHADER_BYTECODE_FILE(filename) STRINGIFY(Debug/##filename)
+#define SHADER_BYTECODE_FILE(filename) STRINGIFY(Intermediate/DXBC-Debug/##filename)
 #elif COMFY_RELEASE
-#define SHADER_BYTECODE_FILE(filename) STRINGIFY(Release/##filename)
+#define SHADER_BYTECODE_FILE(filename) STRINGIFY(Intermediate/DXBC-Release/##filename)
 #else
 #error Unknown build configuration
 #endif /* COMFY_DEBUG / COMFY_RELEASE */
