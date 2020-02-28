0a9277cc463fe55c17aa6a6e27c614ee8de01046
samyuu <samyuu@users.noreply.github.com>
2020-02-28 12:50:29 +0100

    CubeMapCommon +Z / -Y texture coordinates fixes and cleanup

--- ComfyStudio/src/Graphics/Direct3D/ShaderSource/Comfy/Include/CubeMapCommon.hlsl
+++ ComfyStudio/src/Graphics/Direct3D/ShaderSource/Comfy/Include/CubeMapCommon.hlsl
@@ -24,13 +24,19 @@ static const float2 CubeMapNetFaceSize = 1.0 / CubeMapNetDivisions;
 // .... [-Y] .... ....
 static const float2 CubeMapNetFaceRanges[CubeMapFaces] = 
 {
+	// NOTE: [2][1] +X
 	float2(2.0, 1.0) * CubeMapNetFaceSize,
+	// NOTE: [0][1] -X
 	float2(0.0, 1.0) * CubeMapNetFaceSize,
 
+	// NOTE: [1][0] +Y
 	float2(1.0, 0.0) * CubeMapNetFaceSize,
+	// NOTE: [1][2] -Y
 	float2(1.0, 2.0) * CubeMapNetFaceSize,
 
+	// NOTE: [1][2] +Z
 	float2(1.0, 1.0) * CubeMapNetFaceSize,
+	// NOTE: [3][1] -Z
 	float2(3.0, 1.0) * CubeMapNetFaceSize,
 };
 
@@ -51,14 +57,14 @@ float3 GetCubeMapFaceTextureCoordinates(float2 inputTexCoord, int face)
 
 	const float3 faceTextureCoords[CubeMapFaces] = 
 	{
-		float3(+1.0, -inputTexCoord.yx),
-		float3(-1.0, -inputTexCoord.y, +inputTexCoord.x),
-		
+        float3(+1.0, -inputTexCoord.y, +inputTexCoord.x),
+        float3(-1.0, -inputTexCoord.y, +inputTexCoord.x),
+        
         float3(+inputTexCoord.x, +1.0, +inputTexCoord.y),
-		float3(+inputTexCoord.x, -1.0, +inputTexCoord.y),
-		
-        float3(-inputTexCoord.xy, +1.0),
-		float3(-inputTexCoord.xy, -1.0),
+        float3(+inputTexCoord.x, -1.0, -inputTexCoord.y),
+        
+        float3(+inputTexCoord.x, -inputTexCoord.y, +1.0),
+        float3(-inputTexCoord.x, -inputTexCoord.y, -1.0),
 	};
 
 	return faceTextureCoords[face];
