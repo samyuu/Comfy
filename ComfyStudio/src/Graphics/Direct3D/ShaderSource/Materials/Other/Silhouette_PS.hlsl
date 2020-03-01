e89681f20461dc632941e80b33d59ccf4f53abea
samyuu <samyuu@users.noreply.github.com>
2020-03-01 12:37:32 +0100

    Silhouette pixel shader only sample diffuse texture if alpha test is enabled

--- ComfyStudio/src/Graphics/Direct3D/ShaderSource/Materials/Other/Silhouette_PS.hlsl
+++ ComfyStudio/src/Graphics/Direct3D/ShaderSource/Materials/Other/Silhouette_PS.hlsl
@@ -9,7 +9,7 @@ float4 PS_main(VS_OUTPUT_SHADOW input) : SV_Target
 {
     float4 outputColor = float4(0.0, 0.0, 0.0, 1.0);
     
-    if (FLAGS_DIFFUSE_TEX2D)
+    if (FLAGS_DIFFUSE_TEX2D && FLAGS_PUNCH_THROUGH)
     {    
         outputColor.a = DiffuseTexture.Sample(DiffuseSampler, input.TexCoord).a;
         ClipAlphaThreshold(outputColor.a);
