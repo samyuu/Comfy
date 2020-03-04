0f360b82b32a43e4846afba30c40f4bf73eee798
samyuu <samyuu@users.noreply.github.com>
2020-03-04 07:38:01 +0100

    Update SilhouetteOutline pixel shader to resolve compiler warnings

--- ComfyStudio/src/Graphics/Direct3D/Shader/Source/PostProcessing/Other/SilhouetteOutline_PS.hlsl
+++ ComfyStudio/src/Graphics/Direct3D/Shader/Source/PostProcessing/Other/SilhouetteOutline_PS.hlsl
@@ -13,12 +13,14 @@ SamplerState LinearTextureSampler
 
 Texture2D<float> SilhouetteTexture : register(t0);
 
+static const float Threshold = 0.5;
 static const float4 NoColor = float4(0.0, 0.0, 0.0, 0.0);
 static const float4 OutlineColor = float4(0.95, 0.60, 0.16, 1.0);
 
 float4 PS_main(VS_OUTPUT input) : SV_Target
 {
-    if (SilhouetteTexture.Sample(LinearTextureSampler, input.TexCoord) < 0.5)
+#if 0
+    if (SilhouetteTexture.Sample(LinearTextureSampler, input.TexCoord) < Threshold)
     {
         [unroll]
         for (int x = -1; x <= 1; x++)
@@ -28,8 +30,8 @@ float4 PS_main(VS_OUTPUT input) : SV_Target
             {
                 if (x == 0 && y == 0)
                     continue;
-        
-                if (SilhouetteTexture.Sample(LinearTextureSampler, input.TexCoord, int2(x, y)) >= 0.5)
+                
+                if (SilhouetteTexture.Sample(LinearTextureSampler, input.TexCoord, int2(x, y)) >= Threshold)
                     return OutlineColor;
             }
         }
@@ -37,4 +39,22 @@ float4 PS_main(VS_OUTPUT input) : SV_Target
     
     clip(-1.0);
     return NoColor;
+#else
+    // NOTE: Manually unrolled to avoid "Warning X4000: Use of potentially uninitialized variable"
+    if (SilhouetteTexture.Sample(LinearTextureSampler, input.TexCoord) >= Threshold) return OutlineColor;
+    
+    if (SilhouetteTexture.Sample(LinearTextureSampler, input.TexCoord, int2(-1, -1)) >= Threshold) return OutlineColor;
+    if (SilhouetteTexture.Sample(LinearTextureSampler, input.TexCoord, int2(-1, +0)) >= Threshold) return OutlineColor;
+    if (SilhouetteTexture.Sample(LinearTextureSampler, input.TexCoord, int2(-1, +1)) >= Threshold) return OutlineColor;
+
+    if (SilhouetteTexture.Sample(LinearTextureSampler, input.TexCoord, int2(+0, -1)) >= Threshold) return OutlineColor;
+    if (SilhouetteTexture.Sample(LinearTextureSampler, input.TexCoord, int2(+0, +1)) >= Threshold) return OutlineColor;
+    
+    if (SilhouetteTexture.Sample(LinearTextureSampler, input.TexCoord, int2(+1, -1)) >= Threshold) return OutlineColor;
+    if (SilhouetteTexture.Sample(LinearTextureSampler, input.TexCoord, int2(+1, +0)) >= Threshold) return OutlineColor;
+    if (SilhouetteTexture.Sample(LinearTextureSampler, input.TexCoord, int2(+1, +1)) >= Threshold) return OutlineColor;
+    
+    clip(-1.0);
+    return NoColor;
+#endif
 }
