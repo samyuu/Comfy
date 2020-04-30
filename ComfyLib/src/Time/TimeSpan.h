8744b664c490fc7cdd810979e9305eb29f6c58c0
samyuu <samyuu@users.noreply.github.com>
2020-04-30 04:11:21 +0200

    TimeSpan general cleanup

--- ComfyLib/src/Time/TimeSpan.h
+++ ComfyLib/src/Time/TimeSpan.h
@@ -14,9 +14,14 @@ namespace Comfy
 		explicit constexpr TimeSpan(double seconds) : timeInSeconds(seconds) {}
 
 	public:
-		inline constexpr double TotalMinutes() const { return TotalSeconds() / 60.0; }
-		inline constexpr double TotalSeconds() const { return timeInSeconds; }
-		inline constexpr double TotalMilliseconds() const { return TotalSeconds() * 1000.0; }
+		static constexpr TimeSpan FromMinutes(double value) { return TimeSpan(value * 60.0); }
+		static constexpr TimeSpan FromSeconds(double value) { return TimeSpan(value); }
+		static constexpr TimeSpan FromMilliseconds(double value) { return TimeSpan(value / 1000.0); }
+
+	public:
+		constexpr double TotalMinutes() const { return TotalSeconds() / 60.0; }
+		constexpr double TotalSeconds() const { return timeInSeconds; }
+		constexpr double TotalMilliseconds() const { return TotalSeconds() * 1000.0; }
 
 	public:
 		// NOTE: Enough to store "(-)mm:ss:fff"
@@ -28,32 +33,28 @@ namespace Comfy
 		std::string ToString() const;
 
 	public:
-		inline bool operator==(const TimeSpan& other) const { return timeInSeconds == other.timeInSeconds; }
-		inline bool operator!=(const TimeSpan& other) const { return timeInSeconds != other.timeInSeconds; }
-		inline bool operator<=(const TimeSpan& other) const { return timeInSeconds <= other.timeInSeconds; }
-		inline bool operator>=(const TimeSpan& other) const { return timeInSeconds >= other.timeInSeconds; }
-		inline bool operator<(const TimeSpan& other) const { return timeInSeconds < other.timeInSeconds; }
-		inline bool operator>(const TimeSpan& other) const { return timeInSeconds > other.timeInSeconds; }
+		constexpr bool operator==(const TimeSpan& other) const { return timeInSeconds == other.timeInSeconds; }
+		constexpr bool operator!=(const TimeSpan& other) const { return timeInSeconds != other.timeInSeconds; }
+		constexpr bool operator<=(const TimeSpan& other) const { return timeInSeconds <= other.timeInSeconds; }
+		constexpr bool operator>=(const TimeSpan& other) const { return timeInSeconds >= other.timeInSeconds; }
+		constexpr bool operator<(const TimeSpan& other) const { return timeInSeconds < other.timeInSeconds; }
+		constexpr bool operator>(const TimeSpan& other) const { return timeInSeconds > other.timeInSeconds; }
 
-		inline TimeSpan operator+(const TimeSpan other) const { return FromSeconds(TotalSeconds() + other.TotalSeconds()); }
-		inline TimeSpan operator-(const TimeSpan other) const { return FromSeconds(TotalSeconds() - other.TotalSeconds()); }
+		constexpr TimeSpan operator+(const TimeSpan other) const { return FromSeconds(TotalSeconds() + other.TotalSeconds()); }
+		constexpr TimeSpan operator-(const TimeSpan other) const { return FromSeconds(TotalSeconds() - other.TotalSeconds()); }
 
-		inline TimeSpan& operator+=(const TimeSpan& other) { this->timeInSeconds += other.timeInSeconds; return *this; }
-		inline TimeSpan& operator-=(const TimeSpan& other) { this->timeInSeconds -= other.timeInSeconds; return *this; }
+		constexpr TimeSpan& operator+=(const TimeSpan& other) { timeInSeconds += other.timeInSeconds; return *this; }
+		constexpr TimeSpan& operator-=(const TimeSpan& other) { timeInSeconds -= other.timeInSeconds; return *this; }
 
-		inline TimeSpan operator*(double other) const { return FromSeconds(TotalSeconds() * other); }
-		inline TimeSpan operator*(int other) const { return FromSeconds(TotalSeconds() * other); }
+		constexpr TimeSpan operator*(double other) const { return FromSeconds(TotalSeconds() * other); }
+		constexpr TimeSpan operator*(int other) const { return FromSeconds(TotalSeconds() * other); }
 
-		inline double operator/(TimeSpan other) const { return TotalSeconds() / other.TotalSeconds(); }
-		inline double operator/(double other) const { return TotalSeconds() / other; }
-		inline double operator/(int other) const { return TotalSeconds() / other; }
+		constexpr double operator/(TimeSpan other) const { return TotalSeconds() / other.TotalSeconds(); }
+		constexpr double operator/(double other) const { return TotalSeconds() / other; }
+		constexpr double operator/(int other) const { return TotalSeconds() / other; }
 
-		inline TimeSpan operator-() const { return TimeSpan(-timeInSeconds); }
-
-	public:
-		static inline constexpr TimeSpan FromMinutes(double value) { return TimeSpan(value * 60.0); }
-		static inline constexpr TimeSpan FromSeconds(double value) { return TimeSpan(value); }
-		static inline constexpr TimeSpan FromMilliseconds(double value) { return TimeSpan(value / 1000.0); }
+		constexpr TimeSpan operator+() const { return TimeSpan(+timeInSeconds); }
+		constexpr TimeSpan operator-() const { return TimeSpan(-timeInSeconds); }
 
 	public:
 		// NOTE: Must be called once before GetTimeNow() / GetTimeNowAbsolute() can be called
