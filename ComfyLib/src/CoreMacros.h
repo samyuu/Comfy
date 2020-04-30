d5d128acac6e3f413efddf93150bb15489ce2f1c
samyuu <samyuu@users.noreply.github.com>
2020-04-30 04:57:27 +0200

    Implement ScopeExitFunc template struct and COMFY_SCOPE_EXIT macro

--- ComfyLib/src/CoreMacros.h
+++ ComfyLib/src/CoreMacros.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Core/ScopeExitFunc.h"
 
 #define COMFY_CONCAT_DETAIL(x,y) x##y
 #define COMFY_CONCAT(x,y) COMFY_CONCAT_DETAIL(x,y)
@@ -7,6 +8,9 @@
 #define COMFY_STRINGIFY(value) COMFY_STRINGIFY_DETAIL(value)
 
 #define COMFY_UNIQUENAME(prefix) COMFY_CONCAT(prefix, __COUNTER__)
+
+// NOTE: Example: COMFY_SCOPE_EXIT([&] { DoCleanup(); });
+#define COMFY_SCOPE_EXIT(lambda) auto COMFY_UNIQUENAME(__SCOPE_EXIT) = ::Comfy::ScopeExitFunc(lambda)
 
 #if defined(COMFY_DEBUG)
 #define COMFY_DEBUG_ONLY(expression) expression
