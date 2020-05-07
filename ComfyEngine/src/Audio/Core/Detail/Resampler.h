5aa34c1288cac368d5742c36b67261d5a02d799d
samyuu <samyuu@users.noreply.github.com>
2020-05-07 08:04:14 +0200

    Audio Engine remove dummy Resampler classes and implement basic resampling algorithm

--- ComfyEngine/src/Audio/Core/Detail/Resampler.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace Comfy::Audio
-{
-	class Resampler
-	{
-	public:
-	};
-}
---
+++ ComfyEngine/src/Audio/Core/Resample.h
@@ -0,0 +1,100 @@
+#pragma once
+#include "Types.h"
+
+namespace Comfy::Audio
+{
+	namespace Interpolation
+	{
+		struct NearestNeighbor
+		{
+			static constexpr double Interpolate(double startValue, double endValue, double inbetween)
+			{
+				return (inbetween <= 0.5) ? startValue : endValue;
+			}
+		};
+
+		struct Linear
+		{
+			static constexpr double Interpolate(double startValue, double endValue, double inbetween)
+			{
+				return ((1.0 - inbetween) * startValue) + (inbetween * endValue);
+			}
+		};
+
+		struct Cosine
+		{
+			static double Interpolate(double startValue, double endValue, double inbetween)
+			{
+				const auto temp = (1.0 - std::cos(inbetween * glm::pi<double>())) * 0.5;
+				return (startValue * (1.0 - temp) + endValue * temp);
+			}
+		};
+
+		using Best = Cosine;
+	}
+
+	template <typename SampleType>
+	constexpr SampleType SampleAtFrame(i64 atFrame, u32 atChannel, const SampleType* sampels, size_t sampleCount, u32 channelCount)
+	{
+		const auto sampleIndex = static_cast<size_t>((atFrame * channelCount) + atChannel);
+		return sampleIndex < sampleCount ? sampels[sampleIndex] : static_cast<SampleType>(0);
+	}
+
+	template <typename SampleType, typename InterpolationType>
+	constexpr SampleType SampleAtTime(double atSecond, u32 atChannel, const SampleType* samples, size_t sampleCount, double sampleRate, u32 channelCount)
+	{
+		const double frameFaction = (atSecond * sampleRate);
+
+		const i64 startFrame = static_cast<i64>(frameFaction);
+		const i64 endFrame = (startFrame + 1);
+
+		const auto startValue = SampleAtFrame<SampleType>(startFrame, atChannel, samples, sampleCount, channelCount);
+		const auto endValue = SampleAtFrame<SampleType>(endFrame, atChannel, samples, sampleCount, channelCount);
+		const auto inbetween = (frameFaction - static_cast<double>(startFrame));
+
+		constexpr auto maxFloatSampleValue = static_cast<double>(std::numeric_limits<SampleType>::max());
+		const auto normalizedStart = static_cast<double>(startValue / maxFloatSampleValue);
+		const auto normalizedEnd = static_cast<double>(endValue / maxFloatSampleValue);
+
+		const auto normalizedResult = InterpolationType::template Interpolate(normalizedStart, normalizedEnd, inbetween);
+		const auto clampedResult = std::clamp(normalizedResult, -1.0, 1.0);
+
+		const auto sampleTypeResult = static_cast<SampleType>(clampedResult * maxFloatSampleValue);
+		return sampleTypeResult;
+	}
+
+	// NOTE: Relatively low quallity resampling algorithm lacking a low pass filter, should however still be better than having sped up audio.
+	//		 For any serious work it is strongly recommended to resample the audio externally
+	template <typename SampleType, typename InterpolationType = Interpolation::Best>
+	void Resample(std::unique_ptr<SampleType[]>& inOutSamples, size_t& inOutSampleCount, u32& inOutSampleRate, const u32 targetSampleRate, const u32 channelCount)
+	{
+		const auto inSamples = inOutSamples.get();
+		const auto inSampleCount = inOutSampleCount;
+
+		const auto sourceRate = static_cast<double>(inOutSampleRate);
+		const auto targetRate = static_cast<double>(targetSampleRate);
+
+		const auto inFrameCount = (inSampleCount * channelCount);
+		const auto outFrameCount = static_cast<size_t>(inFrameCount * targetRate / sourceRate + 0.5);
+
+		const auto outSampleCount = (outFrameCount * channelCount);
+		auto outSamples = std::make_unique<SampleType[]>(outSampleCount);
+
+		const double outFrameToSecond = (1.0 / targetRate);
+		SampleType* outSampleWriteHead = outSamples.get();
+
+		for (size_t frame = 0; frame < outFrameCount; frame++)
+		{
+			const double second = (static_cast<double>(frame) * outFrameToSecond);
+
+			for (u32 channel = 0; channel < channelCount; channel++)
+				outSampleWriteHead[channel] = SampleAtTime<SampleType, InterpolationType>(second, channel, inSamples, inSampleCount, sourceRate, channelCount);
+
+			outSampleWriteHead += channelCount;
+		}
+
+		inOutSamples = std::move(outSamples);
+		inOutSampleCount = outSampleCount;
+		inOutSampleRate = targetSampleRate;
+	}
+}
