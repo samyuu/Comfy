fdd07d3682af16ced3b6be23cde316987164b192
samyuu <samyuu@users.noreply.github.com>
2020-05-17 10:54:05 +0200

    ComfyEngine start implementing RenderWindow, RenderWindow2D and RenderWindow3D

---
+++ ComfyEngine/src/Window/RenderWindow.cpp
@@ -0,0 +1,184 @@
+#include "RenderWindow.h"
+
+namespace Comfy
+{
+	void RenderWindow::BeginEndGui(const char* windowName, bool* isOpen, ImGuiWindowFlags flags)
+	{
+		constexpr vec2 windowPadding = vec2(2.0f, 2.0f);
+
+		Gui::PushStyleVar(ImGuiStyleVar_WindowPadding, windowPadding);
+		if (Gui::Begin(windowName, isOpen, flags))
+		{
+			Gui::PopStyleVar();
+			Gui::PushID(this);
+
+			CheckUpdateOnFirstFrame();
+			PreRenderTextureGui();
+			Gui::BeginChild("RenderTextureChild##RenderWindow", vec2(0.0f, 0.0f), false, GetRenderTextureChildWindowFlags());
+			RenderTextureGui();
+			Gui::EndChild();
+			PostRenderTextureGui();
+
+			Gui::PopID();
+			Gui::PushStyleVar(ImGuiStyleVar_WindowPadding, windowPadding);
+		}
+		Gui::End();
+		Gui::PopStyleVar();
+	}
+
+	void RenderWindow::BeginEndGuiFullScreen(const char* windowName)
+	{
+		constexpr ImGuiWindowFlags fullscreenWindowFlags =
+			ImGuiWindowFlags_NoDocking |
+			ImGuiWindowFlags_NoTitleBar |
+			ImGuiWindowFlags_NoCollapse |
+			ImGuiWindowFlags_NoResize |
+			ImGuiWindowFlags_NoMove |
+			ImGuiWindowFlags_NoNavFocus |
+			ImGuiWindowFlags_NoSavedSettings;
+
+		const ImGuiViewport* viewport = Gui::GetMainViewport();
+		Gui::SetNextWindowPos(viewport->Pos);
+		Gui::SetNextWindowSize(viewport->Size);
+		Gui::SetNextWindowViewport(viewport->ID);
+
+		Gui::PushStyleVar(ImGuiStyleVar_WindowPadding, vec2(0.0f, 0.0f));
+		Gui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+
+		if (Gui::Begin(windowName, nullptr, fullscreenWindowFlags))
+		{
+			Gui::PushID(this);
+
+			CheckUpdateOnFirstFrame();
+			RenderTextureGui();
+
+			Gui::PopID();
+		}
+		Gui::End();
+		Gui::PopStyleVar(2);
+	}
+
+	vec2 RenderWindow::GetRelativeMouse() const
+	{
+		return Gui::GetMousePos() - renderRegion.Min;
+	}
+
+	bool RenderWindow::GetKeepAspectRatio() const
+	{
+		return keepAspectRatio;
+	}
+
+	void RenderWindow::SetKeepAspectRatio(bool value)
+	{
+		keepAspectRatio = value;
+	}
+
+	float RenderWindow::GetTargetAspectRatio() const
+	{
+		return targetAspectRatio;
+	}
+
+	void RenderWindow::SetTargetAspectRatio(float value)
+	{
+		targetAspectRatio = value;
+	}
+
+	bool RenderWindow::GetWasResized() const
+	{
+		return wasResized;
+	}
+
+	ImRect RenderWindow::GetRenderRegion() const
+	{
+		return renderRegion;
+	}
+
+	void RenderWindow::CheckUpdateOnFirstFrame()
+	{
+		if (!isFirstFrame)
+			return;
+
+		OnFirstFrame();
+		isFirstFrame = false;
+	}
+
+	void RenderWindow::RenderTextureGui()
+	{
+		UpdateRenderRegion();
+		AdjustSizeToTargetAspectRatio();
+
+		const auto renderRegionSize = ivec2(vec2(renderRegion.GetSize()));
+		const auto lastRenderRegionSize = ivec2(vec2(lastRenderRegion.GetSize()));
+
+		if (wasResized = (renderRegionSize != lastRenderRegionSize))
+			needsResizing = true;
+
+		ImGuiWindow* currentWindow = Gui::GetCurrentWindow();
+
+		if (!currentWindow->Hidden)
+		{
+			// NOTE: Skip first few ImGui frames to avoid needlessly resizing while the layout config is still being applied
+			if (needsResizing && Gui::GetFrameCount() >= 2)
+			{
+				OnResize(renderRegionSize);
+				needsResizing = false;
+			}
+
+			OnRender();
+		}
+
+		if (keepAspectRatio)
+		{
+			currentWindow->DrawList->AddRectFilled(
+				fullRenderRegion.Min,
+				fullRenderRegion.Max,
+				Gui::GetColorU32(ImGuiCol_WindowBg));
+		}
+
+		if (const auto textureID = GetTextureID(); textureID != nullptr)
+		{
+			currentWindow->DrawList->AddImage(
+				textureID,
+				renderRegion.GetTL(),
+				renderRegion.GetBR());
+		}
+	}
+
+	void RenderWindow::UpdateRenderRegion()
+	{
+		lastRenderRegion = renderRegion;
+
+		renderRegion.Min = Gui::GetWindowPos();
+		renderRegion.Max = renderRegion.Min + Gui::GetWindowSize();
+
+		fullRenderRegion = renderRegion;
+	}
+
+	void RenderWindow::AdjustSizeToTargetAspectRatio()
+	{
+		if (!keepAspectRatio)
+			return;
+
+		const auto renderRegionSize = vec2(renderRegion.GetSize());
+		const auto renderRegionAspectRatio = renderRegionSize.x / renderRegionSize.y;
+
+		if (renderRegionAspectRatio <= targetAspectRatio) // NOTE: Taller than wide, bars on top / bottom
+		{
+			const auto presentHeight = glm::round((renderRegionSize.x / targetAspectRatio) + 0.5f);
+			const auto barHeight = glm::round((renderRegionSize.y - presentHeight) / 2.0f);
+
+			renderRegion.Min.y += barHeight;
+			renderRegion.Max.y += barHeight;
+			renderRegion.Max.y = renderRegion.Min.y + presentHeight;
+		}
+		else // NOTE: Wider than tall, bars on left / right
+		{
+			const auto presentWidth = static_cast<int>((renderRegionSize.y * targetAspectRatio) + 0.5f);
+			const auto barWidth = static_cast<int>((renderRegionSize.x - presentWidth) / 2.0f);
+
+			renderRegion.Min.x += barWidth;
+			renderRegion.Max.x += barWidth;
+			renderRegion.Max.x = renderRegion.Min.x + presentWidth;
+		}
+	}
+}
---
+++ ComfyEngine/src/Window/RenderWindow.h
@@ -0,0 +1,110 @@
+#pragma once
+#include "Types.h"
+#include "ImGui/Gui.h"
+// #include "Render/Render.h"
+#include <functional>
+
+namespace Comfy
+{
+	class RenderWindow : NonCopyable
+	{
+	public:
+		RenderWindow() = default;
+		virtual ~RenderWindow() = default;
+
+	public:
+		void BeginEndGui(const char* windowName, bool* isOpen = nullptr, ImGuiWindowFlags flags = ImGuiWindowFlags_None);
+
+		// NOTE: Covering the entire main viewport without pre/post gui widgets.
+		//		 It is recommended to use a different window name ID than the non fullscreen version to avoid changing its original layout
+		//		 and making sure no other windows are being drawn after this as they won't be visible
+		void BeginEndGuiFullScreen(const char* windowName);
+
+	public:
+		vec2 GetRelativeMouse() const;
+
+		bool GetKeepAspectRatio() const;
+		void SetKeepAspectRatio(bool value);
+
+		float GetTargetAspectRatio() const;
+		void SetTargetAspectRatio(float value);
+
+		bool GetWasResized() const;
+		ImRect GetRenderRegion() const;
+
+	public:
+		virtual ImTextureID GetTextureID() const = 0;
+
+	protected:
+		virtual ImGuiWindowFlags GetRenderTextureChildWindowFlags() const = 0;
+		virtual void OnFirstFrame() = 0;
+		virtual void PreRenderTextureGui() = 0;
+		virtual void PostRenderTextureGui() = 0;
+		virtual void OnResize(ivec2 newSize) = 0;
+		virtual void OnRender() = 0;
+
+	private:
+		void CheckUpdateOnFirstFrame();
+		void RenderTextureGui();
+		void UpdateRenderRegion();
+		void AdjustSizeToTargetAspectRatio();
+
+	private:
+		bool isFirstFrame = true;
+
+		ImRect renderRegion = {}, lastRenderRegion = {};
+		ImRect fullRenderRegion = {};
+
+		bool wasResized = false;
+		bool needsResizing = true;
+
+		bool keepAspectRatio = false;
+		float targetAspectRatio = (16.0f / 9.0f);
+	};
+
+	class RenderWindow2D : public RenderWindow
+	{
+	public:
+		RenderWindow2D();
+		~RenderWindow2D();
+
+		std::function<void()> OnRenderDebugFunc;
+
+	public:
+		ImTextureID GetTextureID() const override;
+
+	protected:
+		ImGuiWindowFlags GetRenderTextureChildWindowFlags() const override;
+		void OnFirstFrame() override;
+		void PreRenderTextureGui() override;
+		void PostRenderTextureGui() override;
+		void OnResize(ivec2 newSize) override;
+		void OnRender() override;
+
+	private:
+		struct Impl;
+		std::unique_ptr<Impl> impl;
+	};
+
+	class RenderWindow3D : public RenderWindow
+	{
+	public:
+		RenderWindow3D();
+		~RenderWindow3D();
+
+	public:
+		ImTextureID GetTextureID() const override;
+
+	protected:
+		ImGuiWindowFlags GetRenderTextureChildWindowFlags() const override;
+		void OnFirstFrame() override;
+		void PreRenderTextureGui() override;
+		void PostRenderTextureGui() override;
+		void OnResize(ivec2 newSize) override;
+		void OnRender() override;
+
+	private:
+		struct Impl;
+		std::unique_ptr<Impl> impl;
+	};
+}
