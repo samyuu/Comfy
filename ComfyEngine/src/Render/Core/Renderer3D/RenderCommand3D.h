604ea1693a1c9b55e90dd044610dbf34dde10f97
samyuu <samyuu@users.noreply.github.com>
2020-05-20 07:37:13 +0200

    ComfyEngine Renderer3D move RenderCommand3D into its own file

---
+++ ComfyEngine/src/Render/Core/Renderer3D/RenderCommand3D.h
@@ -0,0 +1,84 @@
+#pragma once
+#include "Types.h"
+#include "CoreTypes.h"
+#include "Graphics/Auth3D/ObjSet.h"
+
+namespace Comfy::Render
+{
+	struct RenderCommand3D
+	{
+	public:
+		RenderCommand3D() = default;
+		RenderCommand3D(const Graphics::Obj& obj) : SourceObj(&obj) {}
+		RenderCommand3D(const Graphics::Obj& obj, const vec3& position) : SourceObj(&obj), Transform(position) {}
+
+	public:
+		struct FlagsData
+		{
+			bool IsReflection = false;
+			bool SilhouetteOutline = false;
+
+			bool CastsShadow = false;
+			bool ReceivesShadow = true;
+
+			// TODO:
+			// int ShadowMapIndex = -1;
+			// bool SubsurfaceScattering = false;
+			// bool SubsurfaceScatteringFocusPoint = false;
+			// bool Skeleton = false;
+
+			// NOTE: Optionally render the specified subset of the SourceObj instead
+			int MeshIndex = -1;
+			int SubMeshIndex = -1;
+		};
+
+		struct DynamicData
+		{
+			struct TexturePattern
+			{
+				Cached_TexID SourceID = TexID::Invalid;
+				Cached_TexID OverrideID = TexID::Invalid;
+
+				// NOTE: To easily index into and avoid needless searches
+				std::optional<std::vector<TexID>> CachedIDs;
+			};
+
+			struct TextureTransform
+			{
+				TexID SourceID = TexID::Invalid;
+
+				std::optional<bool> RepeatU, RepeatV;
+				float Rotation = 0.0f;
+				vec2 Translation = vec2(0.0f);
+			};
+
+			struct MaterialOverride
+			{
+				const Graphics::SubMesh* SubMeshToReplace = nullptr;
+				const Graphics::Material* NewMaterial = nullptr;
+			};
+
+			const Graphics::Obj* MorphObj = nullptr;
+			float MorphWeight = 1.0f;
+
+			TexID ScreenRenderTextureID = TexID::Invalid;
+
+			// TODO: Transparency, automatically add to transparent command list
+			// std::optional<vec4> ColorTint;
+
+			std::vector<TexturePattern> TexturePatterns;
+			std::vector<TextureTransform> TextureTransforms;
+
+			std::vector<MaterialOverride> MaterialOverrides;
+		};
+
+	public:
+		const Graphics::Obj* SourceObj = nullptr;
+		Graphics::Transform Transform = Graphics::Transform(vec3(0.0f));
+
+		FlagsData Flags;
+
+		// NOTE: Additional optional data typically animated by A3Ds, stores as a pointer to avoid expensive copies
+		const DynamicData* Dynamic = nullptr;
+	};
+}
--- ComfyEngine/src/Render/Core/Renderer3D/Renderer3D.h
+++ ComfyEngine/src/Render/Core/Renderer3D/Renderer3D.h
@@ -1,10 +1,9 @@
 #pragma once
 #include "Types.h"
 #include "CoreTypes.h"
+#include "RenderCommand3D.h"
 #include "RenderTarget3D.h"
 #include "Render/Core/Camera.h"
-#include "Resource/IDTypes.h"
-#include "Graphics/Auth3D/ObjSet.h"
 #include "Graphics/Auth3D/LightParam/Parameters.h"
 #include <optional>
 #include <functional>
@@ -35,83 +34,6 @@ namespace Comfy::Render
 		Graphics::GlowParameter Glow;
 		Graphics::LightParameter Light;
 		Graphics::IBLParameters IBL;
-	};
-
-	struct RenderCommand3D
-	{
-	public:
-		RenderCommand3D() = default;
-		RenderCommand3D(const Graphics::Obj& obj) : SourceObj(&obj) {}
-		RenderCommand3D(const Graphics::Obj& obj, const vec3& position) : SourceObj(&obj), Transform(position) {}
-
-	public:
-		struct FlagsData
-		{
-			bool IsReflection = false;
-			bool SilhouetteOutline = false;
-
-			bool CastsShadow = false;
-			bool ReceivesShadow = true;
-
-			// TODO:
-			// int ShadowMapIndex = -1;
-			// bool SubsurfaceScattering = false;
-			// bool SubsurfaceScatteringFocusPoint = false;
-			// bool Skeleton = false;
-
-			// NOTE: Optionally render the specified subset of the SourceObj instead
-			int MeshIndex = -1;
-			int SubMeshIndex = -1;
-		};
-
-		struct DynamicData
-		{
-			struct TexturePattern
-			{
-				Cached_TexID SourceID = TexID::Invalid;
-				Cached_TexID OverrideID = TexID::Invalid;
-
-				// NOTE: To easily index into and avoid needless searches
-				std::optional<std::vector<TexID>> CachedIDs;
-			};
-
-			struct TextureTransform
-			{
-				TexID SourceID = TexID::Invalid;
-
-				std::optional<bool> RepeatU, RepeatV;
-				float Rotation = 0.0f;
-				vec2 Translation = vec2(0.0f);
-			};
-
-			struct MaterialOverride
-			{
-				const Graphics::SubMesh* SubMeshToReplace = nullptr;
-				const Graphics::Material* NewMaterial = nullptr;
-			};
-
-			const Graphics::Obj* MorphObj = nullptr;
-			float MorphWeight = 1.0f;
-
-			TexID ScreenRenderTextureID = TexID::Invalid;
-
-			// TODO: Transparency, automatically add to transparent command list
-			// std::optional<vec4> ColorTint;
-
-			std::vector<TexturePattern> TexturePatterns;
-			std::vector<TextureTransform> TextureTransforms;
-
-			std::vector<MaterialOverride> MaterialOverrides;
-		};
-
-	public:
-		const Graphics::Obj* SourceObj = nullptr;
-		Graphics::Transform Transform = Graphics::Transform(vec3(0.0f));
-
-		FlagsData Flags;
-
-		// NOTE: Additional optional data typically animated by A3Ds, stores as a pointer to avoid expensive copies
-		const DynamicData* Dynamic = nullptr;
 	};
 
 	class Renderer3D : NonCopyable
