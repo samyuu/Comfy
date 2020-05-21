db2851ad34f33a4326ce69d0eb943041078e69d5
samyuu <samyuu@users.noreply.github.com>
2020-05-21 03:17:01 +0200

    ComfySandbox Renderer3DTest add minimal test scene

--- ComfySandbox/src/Tests/Renderer3DTest.cpp
+++ ComfySandbox/src/Tests/Renderer3DTest.cpp
@@ -1,4 +1,5 @@
 #include "TestTask.h"
+#include "Resource/ResourceIDMap.h"
 
 namespace Comfy::Sandbox::Tests
 {
@@ -9,12 +10,55 @@ namespace Comfy::Sandbox::Tests
 
 		Renderer3DTest()
 		{
+			const auto[fileContent, fileSize] = IO::File::ReadAllBytes("dev_rom/ibl/tst007.ibl");
+			if (fileContent != nullptr)
+				sceneParam.IBL.Parse(fileContent.get(), fileSize);
+
+			if (objSet != nullptr && texSet != nullptr)
+			{
+				texSet->SetTextureIDs(*objSet);
+
+				for (auto& tex : texSet->Textures)
+					texIDMap.Add(tex->ID, tex);
+			}
 		}
 
 		void Update() override
 		{
+			if (Gui::Begin("Renderer3D Test"))
+			{
+				const vec2 size = Gui::GetContentRegionAvail();
+				camera.AspectRatio = (size.x / size.y);
+				renderTarget->Param.RenderResolution = size;
+
+				renderer.Begin(camera, *renderTarget, sceneParam);
+				{
+					if (objSet != nullptr)
+					{
+						for (auto& obj : *objSet)
+							renderer.Draw(Render::RenderCommand3D(obj));
+					}
+				}
+				renderer.End();
+
+				Gui::Image(renderTarget->GetTextureID(), size);
+			}
+			Gui::End();
 		}
 
 	private:
+		std::unique_ptr<Graphics::ObjSet> objSet = IO::File::Load<Graphics::ObjSet>("dev_rom/objset/copy/stgtst007/stgtst007_obj.bin");
+		std::unique_ptr<Graphics::TexSet> texSet = IO::File::Load<Graphics::TexSet>("dev_rom/objset/copy/stgtst007/stgtst007_tex.bin");
+
+		ResourceIDMap<TexID, Graphics::Tex> texIDMap;
+
+		Render::PerspectiveCamera camera;
+		Render::Renderer3D renderer = Render::TexGetter([&](const Cached_TexID* texID)
+		{
+			return texIDMap.Find(texID);
+		});
+
+		std::unique_ptr<Render::RenderTarget3D> renderTarget = Render::Renderer3D::CreateRenderTarget();
+		Render::SceneParam3D sceneParam;
 	};
 }
