217181cf87fd9d181593fca7ad4208c2b68bb050
samyuu <samyuu@users.noreply.github.com>
2021-06-15 03:17:56 +0200

    ComfyEngine InputSystem fix incorrect button repeat calculations

--- ComfyEngine/src/Input/Core/InputSystem.cpp
+++ ComfyEngine/src/Input/Core/InputSystem.cpp
@@ -495,7 +495,7 @@ namespace Comfy::Input
 					}
 					else if (downDuration > repeatDelay)
 					{
-						const i32 repeatAmount = Gui::CalcTypematicRepeatAmount(downDuration, (downDuration - elapsedTimeSec), repeatDelay, repeatRate);
+						const i32 repeatAmount = Gui::CalcTypematicRepeatAmount((downDuration - elapsedTimeSec), downDuration, repeatDelay, repeatRate);
 						Global.ThisFrameState.ButtonsRepeat[i] = (repeatAmount > 0);
 					}
 				}
